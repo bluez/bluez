d16c23005aaece1792be1f6377655758fe6fc661
Max Krasnyansky <maxk@qualcomm.com>
2002-09-30 06:09:24 +0000

    New RFCOMM ioctl API.

--- include/rfcomm.h
+++ include/rfcomm.h
@@ -49,48 +49,37 @@ struct sockaddr_rc {
 /* RFCOMM TTY support */
 #define RFCOMM_MAX_DEV  256
 
-#define RFCOMM_UNKNOWN  0
-#define RFCOMM_CONNECT  1
-#define RFCOMM_BIND     2
+#define RFCOMMSETMODE		_IOW('R', 200, int)
+#define RFCOMMCREATEDEV		_IOW('R', 201, int)
+#define RFCOMMRELEASEDEV	_IOW('R', 202, int)
+#define RFCOMMGETDEVLIST	_IOR('R', 210, int)
+#define RFCOMMGETDEVINFO	_IOR('R', 211, int)
 
-#define RFCOMMCONNECT		_IOW('R', 201, int)
-#define RFCOMMDISCONNECT	_IOW('R', 202, int)
-#define RFCOMMBIND		_IOW('R', 203, int)
-#define RFCOMMRELEASE		_IOW('R', 204, int)
-
-#define RFCOMMGETDEVLIST	_IOR('R', 211, int)
-#define RFCOMMGETDEVINFO	_IOR('R', 212, int)
-
-struct rfcomm_dev_conn_req {
-	uint16_t dev_id;
-	bdaddr_t src;
-	bdaddr_t dst;
-	uint8_t  chn;
+struct rfcomm_dev_create_req {
+	int16_t      dev_id;
+	bdaddr_t     src;
+	bdaddr_t     dst;
+	uint8_t      channel;
 };
 
-struct rfcomm_dev_disc_req {
-	uint16_t dev_id;
+struct rfcomm_dev_release_req {
+	int16_t      dev_id;
 };
 
-struct rfcomm_dev_data_req {
-	uint16_t dev_id;
-	uint32_t dev_opt;
+struct rfcomm_dev_info {
+	int16_t      id;
+	uint32_t     flags;
+	uint16_t     state;
+	bdaddr_t     src;
+	bdaddr_t     dst;
+	uint8_t      channel;
 };
 
 struct rfcomm_dev_list_req {
-	uint16_t dev_num;
-	struct rfcomm_dev_data_req dev_req[0];
+	uint16_t     dev_num;
+	struct       rfcomm_dev_info dev_info[0];
 };
 
-struct rfcomm_dev_info_req {
-	uint16_t dev_id;
-	uint8_t  type;
-	uint32_t flags;
-	uint16_t state;
-	bdaddr_t src;
-	bdaddr_t dst;
-	uint8_t  chn;
-};
 
 #ifdef __cplusplus
 }
