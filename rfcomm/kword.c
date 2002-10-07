c450127ad7b9705add1c299ec5340e59ff71fbe3
Marcel Holtmann <marcel@holtmann.org>
2002-10-07 07:30:04 +0000

    Correct year in the copyright line

--- rfcomm/kword.c
+++ rfcomm/kword.c
@@ -2,7 +2,7 @@
  *
  *  RFCOMM configuration file
  *
- *  Copyright (C) 2001-2002  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2002  Marcel Holtmann <marcel@holtmann.org>
  *
  *
  *  This program is free software; you can redistribute it and/or modify
