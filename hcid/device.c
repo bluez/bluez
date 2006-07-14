b98c93ed852d29557fe71de5262357b704f85592
Marcel Holtmann <marcel@holtmann.org>
2006-07-14 22:52:04 +0000

    Add Inquiry Result with RSSI support for Kensington dongle

--- hcid/device.c
+++ hcid/device.c
@@ -154,6 +154,8 @@ static inline uint8_t get_inquiry_mode(struct hci_dev *dev)
 			return 1;
 		if (dev->hci_rev == 0x09 && dev->lmp_subver == 0x6963)
 			return 1;
+		if (dev->hci_rev == 0x00 && dev->lmp_subver == 0x6965)
+			return 1;
 	}
 
 	if (dev->manufacturer == 31 &&
