8e1c754a1c9587b0892a218e5eba0f2becdb5965
Marcel Holtmann <marcel@holtmann.org>
2006-08-28 09:12:35 +0000

    hcidump: Print newline for empty commands

--- tools/parser/obex.c
+++ tools/parser/obex.c
@@ -269,8 +269,10 @@ void obex_dump(int level, struct frame *frm)
 
 		set_status(frm->handle, frm->dlci, status);
 
-		if (frm->len == 0)
+		if (frm->len == 0) {
+			printf("\n");
 			break;
+		}
 
 		switch (opcode & 0x7f) {
 		case 0x00:	/* Connect */
