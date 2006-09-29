d78dcb8b79e0db5dddab5f98cd825c69f0bf9938
Marcel Holtmann <marcel@holtmann.org>
2006-09-29 19:11:48 +0000

    Add experimental objects

--- test/dbusdef.py
+++ test/dbusdef.py
@@ -5,3 +5,9 @@ bus = dbus.SystemBus()
 manager = dbus.Interface(bus.get_object('org.bluez', '/org/bluez'), 'org.bluez.Manager')
 
 adapter = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.Adapter')
+
+test = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.Test')
+
+rfcomm = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.RFCOMM')
+
+sdp = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.SDP')
