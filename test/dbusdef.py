b467488a2ed54badfff71b4aa892ded051670aa3
Marcel Holtmann <marcel@holtmann.org>
2006-10-29 16:50:26 +0000

    Add service objects

--- test/dbusdef.py
+++ test/dbusdef.py
@@ -11,3 +11,18 @@ test = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'or
 rfcomm = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.RFCOMM')
 
 sdp = dbus.Interface(bus.get_object('org.bluez', manager.DefaultAdapter()), 'org.bluez.SDP')
+
+echo = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/echo'), 'org.bluez.Service')
+
+network = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/network'), 'org.bluez.Service')
+
+input = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/input'), 'org.bluez.Service')
+
+audio = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/audio'), 'org.bluez.Service')
+
+def connect_echo() :
+	return dbus.Interface(bus.get_object(echo.GetConnectionName(), '/org/bluez/echo'), 'org.freedesktop.DBus.Introspectable')
+
+def connect_holtmann() :
+	holtmann = dbus.Interface(bus.get_object('org.bluez', '/org/holtmann'), 'org.bluez.Service')
+	return dbus.Interface(bus.get_object(holtmann.GetConnectionName(), '/org/holtmann'), 'org.freedesktop.DBus.Introspectable')
