c7d02264b409fb1fe1af55a45417cf39a2362f66
Marcel Holtmann <marcel@holtmann.org>
2007-02-14 09:25:40 +0000

    Typo

--- tools/hid2hci.c
+++ tools/hid2hci.c
@@ -226,7 +226,7 @@ static struct device_id device_list[] = {
 	{ HCI, 0x046d, 0xc70a, switch_logitech },	/* Logitech diNovo mouse */
 	{ HCI, 0x046d, 0xc70b, switch_logitech },	/* Logitech diNovo Laser keyboard */
 	{ HCI, 0x046d, 0xc70c, switch_logitech },	/* Logitech diNovo Laser mouse */
-	{ HCI, 0x046d, 0xc70e, switch_logitech },	/* logitech diNovo keyboard */
+	{ HCI, 0x046d, 0xc70e, switch_logitech },	/* Logitech diNovo keyboard */
 	{ -1 }
 };
 
