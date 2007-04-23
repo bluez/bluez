71889d8638c31b0274e0a279b7879cd96aef7a50
Claudio Takahasi <claudio.takahasi@openbossa.org>
2007-04-23 14:19:56 +0000

    network: missing set the D-Bus connection when creating a server from file

--- network/server.c
+++ network/server.c
@@ -1117,6 +1117,7 @@ int server_register_from_file(DBusConnection *conn, const char *path,
 	bacpy(&ns->src, src);
 	ns->path = g_strdup(path);
 	ns->id = id;
+	ns->conn = dbus_connection_ref(conn);
 	ns->name = textfile_get(filename, "name");
 	if (!ns->name) {
 		/* Name is mandatory */
