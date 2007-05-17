a4bf1139272498ac3058959b60e19b967efc1b73
Marcel Holtmann <marcel@holtmann.org>
2007-05-17 16:21:31 +0000

    Fix some ppoll issue

--- common/ppoll.h
+++ common/ppoll.h
@@ -1,4 +1,10 @@
-static inline int ppoll(struct pollfd *fds, nfds_t nfds,
+#ifdef ppoll
+#undef ppoll
+#endif
+
+#define ppoll compat_ppoll
+
+static inline int compat_ppoll(struct pollfd *fds, nfds_t nfds,
 		const struct timespec *timeout, const sigset_t *sigmask)
 {
 	return poll(fds, nfds, timeout ? timeout->tv_sec * 1000 : 500);
