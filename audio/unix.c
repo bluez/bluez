31bd0bed86998f29e89061e5a3b0925c0d2f52a5
Marcel Holtmann <marcel@holtmann.org>
2007-08-30 00:02:37 +0000

    Change the default bitpool range to 2-32

--- audio/unix.c
+++ audio/unix.c
@@ -538,7 +538,7 @@ static int cfg_to_caps(struct ipc_data_cfg *cfg, struct sbc_codec_cap *sbc_cap)
 	else if (sbc->bitpool > 0)
 		sbc_cap->min_bitpool = sbc_cap->max_bitpool = sbc->bitpool;
 	else {
-		sbc->bitpool = 53;
+		sbc->bitpool = 32;
 		sbc_cap->min_bitpool = 2;
 		sbc_cap->max_bitpool = sbc->bitpool;
 	}
