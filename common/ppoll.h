b866782105bd76c0f7f2c2bc9e2f4a25d3b0d0fe
Marcel Holtmann <marcel@holtmann.org>
2008-03-07 14:17:56 +0000

    Fix handling of compat_ppoll when no timeout is specified

--- common/ppoll.h
+++ common/ppoll.h
@@ -7,5 +7,10 @@
 static inline int compat_ppoll(struct pollfd *fds, nfds_t nfds,
 		const struct timespec *timeout, const sigset_t *sigmask)
 {
-	return poll(fds, nfds, timeout ? timeout->tv_sec * 1000 : 500);
+	if (timeout == NULL)
+		return poll(fds, nfds, -1);
+	else if (timeout->tv_sec == 0)
+		return poll(fds, nfds, 500);
+	else
+		return poll(fds, nfds, timeout->tv_sec * 1000);
 }
