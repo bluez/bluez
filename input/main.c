a93a6fa2f4091679556db6ddd65b3291592fe9c9
Marcel Holtmann <marcel@holtmann.org>
2008-05-29 21:16:32 +0000

    Remove CancelConnect implementation

--- input/main.c
+++ input/main.c
@@ -45,12 +45,6 @@ static DBusMessage *input_connect(DBusConnection *conn,
 	return g_dbus_create_reply(msg, DBUS_TYPE_INVALID);
 }
 
-static DBusMessage *input_cancel_connect(DBusConnection *conn,
-					DBusMessage *msg, void *user_data)
-{
-	return g_dbus_create_reply(msg, DBUS_TYPE_INVALID);
-}
-
 static DBusMessage *input_disconnect(DBusConnection *conn,
 					DBusMessage *msg, void *user_data)
 {
@@ -67,10 +61,9 @@ static DBusMessage *input_is_connected(DBusConnection *conn,
 }
 
 static GDBusMethodTable input_methods[] = {
-	{ "Connect",       "", "",  input_connect        },
-	{ "CancelConnect", "", "",  input_cancel_connect },
-	{ "Disconnect",    "", "",  input_disconnect     },
-	{ "IsConnected",   "", "b", input_is_connected   },
+	{ "Connect",     "", "",  input_connect      },
+	{ "Disconnect",  "", "",  input_disconnect   },
+	{ "IsConnected", "", "b", input_is_connected },
 	{ }
 };
 
