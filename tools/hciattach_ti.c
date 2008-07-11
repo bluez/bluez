8af2789f18e64aaee0ae52680f4474814b0be0b8
Marcel Holtmann <marcel@holtmann.org>
2008-07-11 08:46:06 +0000

    Add the TI copyright note

--- tools/hciattach_ti.c
+++ tools/hciattach_ti.c
@@ -2,6 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
+ *  Copyright (C) 2007-2008  Texas Instruments, Inc.
  *  Copyright (C) 2005-2008  Marcel Holtmann <marcel@holtmann.org>
  *
  *
