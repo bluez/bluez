275d894e0f72bd4249b936fb0f2d46fbfb6c20db
Marcel Holtmann <marcel@holtmann.org>
2008-09-01 22:22:37 +0200

    Move service record registration into adapter

--- src/adapter.c
+++ src/adapter.c
@@ -50,12 +50,12 @@
 
 #include "hcid.h"
 #include "sdpd.h"
+#include "sdp-xml.h"
 #include "manager.h"
 #include "adapter.h"
 #include "device.h"
 #include "dbus-common.h"
 #include "dbus-hci.h"
-#include "dbus-database.h"
 #include "error.h"
 #include "glib-helper.h"
 #include "agent.h"
@@ -75,8 +75,8 @@ static DBusConnection *connection = NULL;
 static GSList *adapter_drivers = NULL;
 
 struct record_list {
-  sdp_list_t *recs;
-  const gchar *addr;
+	sdp_list_t *recs;
+	const gchar *addr;
 };
 
 struct session_req {
@@ -92,6 +92,27 @@ struct service_auth {
 	service_auth_cb cb;
 	void *user_data;
 };
+
+static GSList *records = NULL;
+
+struct record_data {
+	uint32_t handle;
+	char *sender;
+	guint listener_id;
+};
+
+static struct record_data *find_record(uint32_t handle, const char *sender)
+{
+	GSList *list;
+
+	for (list = records; list; list = list->next) {
+		struct record_data *data = list->data;
+		if (handle == data->handle && !strcmp(sender, data->sender))
+			return data;
+	}
+
+	return NULL;
+}
 
 static inline DBusMessage *invalid_args(DBusMessage *msg)
 {
@@ -120,7 +141,7 @@ static inline DBusMessage *no_such_adapter(DBusMessage *msg)
 static inline DBusMessage *failed_strerror(DBusMessage *msg, int err)
 {
 	return g_dbus_create_error(msg, ERROR_INTERFACE ".Failed",
-			strerror(err));
+							strerror(err));
 }
 
 static inline DBusMessage *in_progress(DBusMessage *msg, const char *str)
@@ -1887,6 +1908,56 @@ static DBusMessage *unregister_agent(DBusConnection *conn,
 	return dbus_message_new_method_return(msg);
 }
 
+static void exit_callback(void *user_data)
+{
+	struct record_data *user_record = user_data;
+
+	debug("remove record");
+
+	records = g_slist_remove(records, user_record);
+
+	remove_record_from_server(user_record->handle);
+
+	g_free(user_record->sender);
+	g_free(user_record);
+}
+
+static int add_xml_record(DBusConnection *conn, const char *sender,
+		bdaddr_t *src, const char *record, dbus_uint32_t *handle)
+{
+	struct record_data *user_record;
+	sdp_record_t *sdp_record;
+
+	sdp_record = sdp_xml_parse_record(record, strlen(record));
+	if (!sdp_record) {
+		error("Parsing of XML service record failed");
+		return -EIO;
+	}
+
+	if (add_record_to_server(src, sdp_record) < 0) {
+		error("Failed to register service record");
+		sdp_record_free(sdp_record);
+		return -EIO;
+	}
+
+	user_record = g_new0(struct record_data, 1);
+
+	user_record->handle = sdp_record->handle;
+
+	user_record->sender = g_strdup(sender);
+
+	records = g_slist_append(records, user_record);
+
+	user_record->listener_id = g_dbus_add_disconnect_watch(conn, sender,
+					exit_callback, user_record, NULL);
+
+	debug("listener_id %d", user_record->listener_id);
+
+	*handle = user_record->handle;
+
+	return 0;
+}
+
 static DBusMessage *add_service_record(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
@@ -1917,6 +1988,68 @@ static DBusMessage *add_service_record(DBusConnection *conn,
 	return reply;
 }
 
+static DBusMessage *update_record(DBusConnection *conn, DBusMessage *msg,
+		bdaddr_t *src, dbus_uint32_t handle, sdp_record_t *sdp_record)
+{
+	int err;
+
+	if (remove_record_from_server(handle) < 0) {
+		sdp_record_free(sdp_record);
+		return g_dbus_create_error(msg,
+				ERROR_INTERFACE ".NotAvailable",
+				"Not Available");
+	}
+
+	sdp_record->handle = handle;
+	err = add_record_to_server(src, sdp_record);
+	if (err < 0) {
+		sdp_record_free(sdp_record);
+		error("Failed to update the service record");
+		return g_dbus_create_error(msg,
+				ERROR_INTERFACE ".Failed",
+				strerror(EIO));
+	}
+
+	return dbus_message_new_method_return(msg);
+}
+
+static DBusMessage *update_xml_record(DBusConnection *conn,
+					DBusMessage *msg, bdaddr_t *src)
+{
+	struct record_data *user_record;
+	sdp_record_t *sdp_record;
+	const char *record;
+	dbus_uint32_t handle;
+	int len;
+
+	if (dbus_message_get_args(msg, NULL,
+				DBUS_TYPE_UINT32, &handle,
+				DBUS_TYPE_STRING, &record,
+				DBUS_TYPE_INVALID) == FALSE)
+		return NULL;
+
+	len = (record ? strlen(record) : 0);
+	if (len == 0)
+		return invalid_args(msg);
+
+	user_record = find_record(handle, dbus_message_get_sender(msg));
+	if (!user_record)
+		return g_dbus_create_error(msg,
+				ERROR_INTERFACE ".NotAvailable",
+				"Not Available");
+
+	sdp_record = sdp_xml_parse_record(record, len);
+	if (!sdp_record) {
+		error("Parsing of XML service record failed");
+		sdp_record_free(sdp_record);
+		return g_dbus_create_error(msg,
+				ERROR_INTERFACE ".Failed",
+				strerror(EIO));
+	}
+
+	return update_record(conn, msg, src, handle, sdp_record);
+}
+
 static DBusMessage *update_service_record(DBusConnection *conn,
 						DBusMessage *msg, void *data)
 {
@@ -1926,6 +2059,26 @@ static DBusMessage *update_service_record(DBusConnection *conn,
 	str2ba(adapter->address, &src);
 
 	return update_xml_record(conn, msg, &src);
+}
+
+static int remove_record(DBusConnection *conn, const char *sender,
+							dbus_uint32_t handle)
+{
+	struct record_data *user_record;
+
+	debug("remove record 0x%x", handle);
+
+	user_record = find_record(handle, sender);
+	if (!user_record)
+		return -1;
+
+	debug("listner_id %d", user_record->listener_id);
+
+	g_dbus_remove_watch(conn, user_record->listener_id);
+
+	exit_callback(user_record);
+
+	return 0;
 }
 
 static DBusMessage *remove_service_record(DBusConnection *conn,
