d55b2b10927a7ac07c82c50be6216c32711d94a6
Marcel Holtmann <marcel@holtmann.org>
2008-09-14 19:13:43 +0200

    Don't bother with the source value for input devices

--- input/device.c
+++ input/device.c
@@ -606,7 +606,6 @@ static int hidp_add_connection(const struct input_device *idev,
 	struct fake_hid *fake_hid;
 	struct fake_input *fake;
 	sdp_record_t *rec;
-	uint16_t source;
 	char src_addr[18], dst_addr[18];
 	int err;
 
@@ -629,7 +628,7 @@ static int hidp_add_connection(const struct input_device *idev,
 	extract_hid_record(rec, req);
 	sdp_record_free(rec);
 
-	read_device_id(src_addr, dst_addr, &source,
+	read_device_id(src_addr, dst_addr, NULL,
 				&req->vendor, &req->product, &req->version);
 
 	fake_hid = get_fake_hid(req->vendor, req->product);
