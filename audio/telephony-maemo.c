e1486fd2598012504121342e80fd2c37aaadf17b
Johan Hedberg <johan.hedberg@nokia.com>
2008-10-16 10:52:53 +0300

    Starting point for the maemo telephony plugin
    This is mostly a copy-paste from telephony-dummy.c with the TelephonyTest D-Bus
    interface removed.

--- audio/telephony-maemo.c
+++ audio/telephony-maemo.c
@@ -25,3 +25,181 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <glib.h>
+#include <dbus/dbus.h>
+#include <gdbus.h>
+
+#include "logging.h"
+#include "telephony.h"
+
+static DBusConnection *connection = NULL;
+
+static char *subscriber_number = NULL;
+static char *active_call_number = NULL;
+static int active_call_status = 0;
+static int active_call_dir = 0;
+
+static gboolean events_enabled = FALSE;
+
+/* Response and hold state
+ * -1 = none
+ *  0 = incoming call is put on hold in the AG
+ *  1 = held incoming call is accepted in the AG
+ *  2 = held incoming call is rejected in the AG
+ */
+static int response_and_hold = -1;
+
+static struct indicator dummy_indicators[] =
+{
+	{ "battchg",	"0-5",	5 },
+	{ "signal",	"0-5",	5 },
+	{ "service",	"0,1",	1 },
+	{ "call",	"0,1",	0 },
+	{ "callsetup",	"0-3",	0 },
+	{ "callheld",	"0-2",	0 },
+	{ "roam",	"0,1",	0 },
+	{ NULL }
+};
+
+void telephony_device_connected(void *telephony_device)
+{
+	debug("telephony-maemo: device %p connected", telephony_device);
+}
+
+void telephony_device_disconnected(void *telephony_device)
+{
+	debug("telephony-maemo: device %p disconnected", telephony_device);
+	events_enabled = FALSE;
+}
+
+void telephony_event_reporting_req(void *telephony_device, int ind)
+{
+	events_enabled = ind == 1 ? TRUE : FALSE;
+
+	telephony_event_reporting_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+void telephony_response_and_hold_req(void *telephony_device, int rh)
+{
+	response_and_hold = rh;
+
+	telephony_response_and_hold_ind(response_and_hold);
+
+	telephony_response_and_hold_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+void telephony_last_dialed_number_req(void *telephony_device)
+{
+	telephony_last_dialed_number_rsp(telephony_device, CME_ERROR_NONE);
+
+	/* Notify outgoing call set-up successfully initiated */
+	telephony_update_indicator(dummy_indicators, "callsetup",
+					EV_CALLSETUP_OUTGOING);
+	telephony_update_indicator(dummy_indicators, "callsetup",
+					EV_CALLSETUP_ALERTING);
+
+	active_call_status = CALL_STATUS_ALERTING;
+	active_call_dir = CALL_DIR_OUTGOING;
+}
+
+void telephony_terminate_call_req(void *telephony_device)
+{
+	g_free(active_call_number);
+	active_call_number = NULL;
+
+	telephony_terminate_call_rsp(telephony_device, CME_ERROR_NONE);
+
+	if (telephony_get_indicator(dummy_indicators, "callsetup") > 0)
+		telephony_update_indicator(dummy_indicators, "callsetup",
+						EV_CALLSETUP_INACTIVE);
+	else
+		telephony_update_indicator(dummy_indicators, "call",
+						EV_CALL_INACTIVE);
+}
+
+void telephony_answer_call_req(void *telephony_device)
+{
+	telephony_answer_call_rsp(telephony_device, CME_ERROR_NONE);
+
+	telephony_update_indicator(dummy_indicators, "call", EV_CALL_ACTIVE);
+	telephony_update_indicator(dummy_indicators, "callsetup",
+					EV_CALLSETUP_INACTIVE);
+
+	active_call_status = CALL_STATUS_ACTIVE;
+}
+
+void telephony_dial_number_req(void *telephony_device, const char *number)
+{
+	g_free(active_call_number);
+	active_call_number = g_strdup(number);
+
+	debug("telephony-maemo: dial request to %s", active_call_number);
+
+	telephony_dial_number_rsp(telephony_device, CME_ERROR_NONE);
+
+	/* Notify outgoing call set-up successfully initiated */
+	telephony_update_indicator(dummy_indicators, "callsetup",
+					EV_CALLSETUP_OUTGOING);
+	telephony_update_indicator(dummy_indicators, "callsetup",
+					EV_CALLSETUP_ALERTING);
+
+	active_call_status = CALL_STATUS_ALERTING;
+	active_call_dir = CALL_DIR_OUTGOING;
+}
+
+void telephony_transmit_dtmf_req(void *telephony_device, char tone)
+{
+	debug("telephony-maemo: transmit dtmf: %c", tone);
+	telephony_transmit_dtmf_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+void telephony_subscriber_number_req(void *telephony_device)
+{
+	debug("telephony-maemo: subscriber number request");
+	if (subscriber_number)
+		telephony_subscriber_number_ind(subscriber_number, 0,
+						SUBSCRIBER_SERVICE_VOICE);
+	telephony_subscriber_number_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+void telephony_list_current_calls_req(void *telephony_device)
+{
+	debug("telephony-maemo: list current calls request");
+	if (active_call_number)
+		telephony_list_current_call_ind(1, active_call_dir,
+						active_call_status,
+						CALL_MODE_VOICE,
+						CALL_MULTIPARTY_NO,
+						active_call_number,
+						0);
+	telephony_list_current_calls_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+void telephony_operator_selection_req(void *telephony_device)
+{
+	telephony_operator_selection_ind(OPERATOR_MODE_AUTO, "DummyOperator");
+	telephony_operator_selection_rsp(telephony_device, CME_ERROR_NONE);
+}
+
+int telephony_init(void)
+{
+	uint32_t features = AG_FEATURE_REJECT_A_CALL |
+				AG_FEATURE_ENHANCED_CALL_STATUS |
+				AG_FEATURE_EXTENDED_ERROR_RESULT_CODES;
+
+	connection = dbus_bus_get(DBUS_BUS_SYSTEM, NULL);
+
+	telephony_ready_ind(features, dummy_indicators, response_and_hold);
+
+	return 0;
+}
+
+void telephony_exit(void)
+{
+	dbus_connection_unref(connection);
+	connection = NULL;
+}
