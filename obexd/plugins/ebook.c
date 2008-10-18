30d230d81df0a41cff6841dafa96b10bf00b3251
Marcel Holtmann <marcel@holtmann.org>
2008-10-18 07:33:40 +0200

    obexd: Add skeleton for EBook plugin

---
+++ obexd/plugins/ebook.c
@@ -0,0 +1,45 @@
+/*
+ *
+ *  OBEX Server
+ *
+ *  Copyright (C) 2007-2008  Marcel Holtmann <marcel@holtmann.org>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <plugin.h>
+#include <logging.h>
+
+#include <libebook/e-book.h>
+
+static int ebook_init(void)
+{
+	DBG("");
+
+	return 0;
+}
+
+static void ebook_exit(void)
+{
+	DBG("");
+}
+
+OBEX_PLUGIN_DEFINE("ebook", ebook_init, ebook_exit)
