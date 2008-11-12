817a88bd9158120f9c687eec55b1ee30897f8a98
Johan Hedberg <johan.hedberg@nokia.com>
2008-11-12 13:37:50 +0200

    Remove prototype for non-existent function

--- src/dbus-hci.h
+++ src/dbus-hci.h
@@ -47,8 +47,6 @@ int hcid_dbus_user_passkey(bdaddr_t *sba, bdaddr_t *dba);
 int hcid_dbus_user_notify(bdaddr_t *sba, bdaddr_t *dba, uint32_t passkey);
 void hcid_dbus_link_key_notify(bdaddr_t *local, bdaddr_t *peer);
 
-int unregister_adapter_path(const char *path);
-
 DBusMessage *new_authentication_return(DBusMessage *msg, uint8_t status);
 
 int cancel_discovery(struct btd_adapter *adapter);
