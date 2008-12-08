24b10e587ce2092197ea2b6239b9b76fb5f82998
Johan Hedberg <johan.hedberg@nokia.com>
2008-12-08 18:20:16 +0200

    Fix copyright year

--- audio/telephony-maemo.c
+++ audio/telephony-maemo.c
@@ -2,7 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
- *  Copyright (C) 2006-2007  Nokia Corporation
+ *  Copyright (C) 2008  Nokia Corporation
  *  Copyright (C) 2004-2008  Marcel Holtmann <marcel@holtmann.org>
  *
  *
