abdf12d1ad67ab373247036eb9efdb48aff9a9c3
Johan Hedberg <johan.hedberg@nokia.com>
2009-03-23 11:09:11 +0200

    Use comma as list separator in audio.conf for consistency with main.conf

--- audio/main.c
+++ audio/main.c
@@ -55,6 +55,8 @@ static GKeyFile *load_config_file(const char *file)
 
 	keyfile = g_key_file_new();
 
+	g_key_file_set_list_separator(keyfile, ',');
+
 	if (!g_key_file_load_from_file(keyfile, file, 0, &err)) {
 		error("Parsing %s failed: %s", file, err->message);
 		g_error_free(err);
