33c1047e91442c957241bc5a89e826c7e335853a
Marcel Holtmann <marcel@holtmann.org>
2009-07-16 08:42:06 +0200

    Fix strict-aliasing rules handling with CMSG_DATA

--- audio/ipc.c
+++ audio/ipc.c
@@ -106,8 +106,10 @@ int bt_audio_service_get_data_fd(int sk)
 	for (cmsg = CMSG_FIRSTHDR(&msgh); cmsg != NULL;
 			cmsg = CMSG_NXTHDR(&msgh, cmsg)) {
 		if (cmsg->cmsg_level == SOL_SOCKET
-				&& cmsg->cmsg_type == SCM_RIGHTS)
-			return (*(int *) CMSG_DATA(cmsg));
+				&& cmsg->cmsg_type == SCM_RIGHTS) {
+			memcpy(&ret, CMSG_DATA(cmsg), sizeof(int));
+			return ret;
+		}
 	}
 
 	errno = EINVAL;
