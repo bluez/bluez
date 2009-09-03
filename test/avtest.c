0ed3510c9dd825e2f1b1ada2c4dd089d6fd10de9
Johan Hedberg <johan.hedberg@nokia.com>
2009-09-03 11:49:11 +0300

    Add Nokia copyright to avtest.c

--- test/avtest.c
+++ test/avtest.c
@@ -3,6 +3,7 @@
  *  BlueZ - Bluetooth protocol stack for Linux
  *
  *  Copyright (C) 2007-2009  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2009  Nokia Corporation
  *
  *
  *  This program is free software; you can redistribute it and/or modify
