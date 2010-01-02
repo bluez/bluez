4174e904a215173afb94149ff8800262e60b3b9c
Marcel Holtmann <marcel@holtmann.org>
2010-01-01 17:09:14 -0800

    obexd: Update copyright information

--- obexd/plugins/phonebook-dummy.c
+++ obexd/plugins/phonebook-dummy.c
@@ -3,7 +3,7 @@
  *  OBEX Server
  *
  *  Copyright (C) 2009  Intel Corporation
- *  Copyright (C) 2007-2009  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2007-2010  Marcel Holtmann <marcel@holtmann.org>
  *
  *
  *  This program is free software; you can redistribute it and/or modify
--- obexd/src/btio.h
+++ obexd/src/btio.h
@@ -2,7 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
- *  Copyright (C) 2009  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2010  Marcel Holtmann <marcel@holtmann.org>
  *  Copyright (C) 2009  Nokia Corporation
  *
  *
