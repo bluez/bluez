d3b613c392352f59fd6b18becf80e6504ad2423c
Marcel Holtmann <marcel@holtmann.org>
2010-01-07 01:12:16 -0800

    Update Intel copyrights

--- audio/telephony-ofono.c
+++ audio/telephony-ofono.c
@@ -2,7 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
- *  Copyright (C) 2009       Intel Corporation
+ *  Copyright (C) 2009-2010  Intel Corporation
  *  Copyright (C) 2006-2009  Nokia Corporation
  *  Copyright (C) 2004-2010  Marcel Holtmann <marcel@holtmann.org>
  *
