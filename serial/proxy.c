9f71c68cc547f56c343d04f9c96d6fb27bde5050
Gustavo F. Padovan <gustavo@padovan.org>
2010-05-21 09:26:16 -0300

    Remove old defines from serial code

--- serial/proxy.c
+++ serial/proxy.c
@@ -63,12 +63,6 @@
 #include "btio.h"
 #include "proxy.h"
 
-#define SERIAL_PORT_NAME	"spp"
-#define SERIAL_PORT_UUID	"00001101-0000-1000-8000-00805F9B34FB"
-
-#define DIALUP_NET_NAME		"dun"
-#define DIALUP_NET_UUID		"00001103-0000-1000-8000-00805F9B34FB"
-
 #define SERIAL_PROXY_INTERFACE	"org.bluez.SerialProxy"
 #define SERIAL_MANAGER_INTERFACE "org.bluez.SerialProxyManager"
 #define BUF_SIZE		1024
