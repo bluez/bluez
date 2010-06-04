69394a48e07b678a778527c44539c318ac349289
Johan Hedberg <johan.hedberg@nokia.com>
2010-06-04 16:56:53 +0300

    obexd: Fix copyright statements in nokia-backup.c

--- obexd/plugins/nokia-backup.c
+++ obexd/plugins/nokia-backup.c
@@ -2,8 +2,8 @@
  *
  *  OBEX Server
  *
- *  Copyright (C) 2009-2010  Intel Corporation
- *  Copyright (C) 2007-2010  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2010  Nokia Corporation
+ *  Copyright (C) 2010  Marcel Holtmann <marcel@holtmann.org>
  *
  *
  *  This program is free software; you can redistribute it and/or modify
