e33cf832d96a907deabb0cd1bf80860e8043ede5
Marcel Holtmann <marcel@holtmann.org>
2010-08-19 20:03:38 +0200

    obexd: Sort IrMC after PBAP

--- obexd/src/obex.h
+++ obexd/src/obex.h
@@ -33,8 +33,8 @@
 #define OBEX_FTP	(1 << 2)
 #define OBEX_BIP	(1 << 3)
 #define OBEX_PBAP	(1 << 4)
-#define OBEX_PCSUITE	(1 << 5)
-#define OBEX_IRMC	(1 << 6)
+#define OBEX_IRMC	(1 << 5)
+#define OBEX_PCSUITE	(1 << 6)
 #define OBEX_SYNCEVOLUTION	(1 << 7)
 
 #define TARGET_SIZE 16
