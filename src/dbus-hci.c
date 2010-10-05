bluez/bluez

commit e7daece858070d71cecf6ade4f0e3c93272c53ac

Move PIN code reply to hciops

src/dbus-hci.c
@@ -160,45 +160,23 @@ const char *class_to_icon(uint32_t class)
  *
  *****************************************************************/
 
-static void pincode_cb(struct agent *agent, DBusError *err,
+static void pincode_cb(struct agent *agent, DBusError *derr,
 				const char *pincode, struct btd_device *device)
 {
 	struct btd_adapter *adapter = device_get_adapter(device);
-	pin_code_reply_cp pr;
 	bdaddr_t sba, dba;
-	size_t len;
-	int dev;
-	uint16_t dev_id = adapter_get_dev_id(adapter);
+	int err;
 
-	dev = hci_open_dev(dev_id);
-	if (dev < 0) {
-		error("hci_open_dev(%d): %s (%d)", dev_id,
-				strerror(errno), errno);
+	device_get_address(device, &dba);
+
+	err = btd_adapter_pincode_reply(adapter, &dba, derr ? pincode : NULL);
+	if (err < 0) {
+		error("pincode_reply: %s (%d)", strerror(-err), -err);
 		return;
 	}
 
-	adapter_get_address(adapter, &sba);
-	device_get_address(device, &dba);
-
-	if (err) {
-		hci_send_cmd(dev, OGF_LINK_CTL,
-				OCF_PIN_CODE_NEG_REPLY, 6, &dba);
-		goto done;
-	}
-
-	len = strlen(pincode);
-
-	set_pin_length(&sba, len);
-
-	memset(&pr, 0, sizeof(pr));
-	bacpy(&pr.bdaddr, &dba);
-	memcpy(pr.pin_code, pincode, len);
-	pr.pin_len = len;
-	hci_send_cmd(dev, OGF_LINK_CTL, OCF_PIN_CODE_REPLY,
-						PIN_CODE_REPLY_CP_SIZE, &pr);
-
-done:
-	hci_close_dev(dev);
+	if (derr == NULL)
+		set_pin_length(&sba, strlen(pincode));
 }
 
 int hcid_dbus_request_pin(int dev, bdaddr_t *sba, struct hci_conn_info *ci)

plugins/hciops.c
@@ -1108,6 +1108,33 @@ fail:
 	return err;
 }
 
+static int hciops_pincode_reply(int index, bdaddr_t *bdaddr, const char *pin)
+{
+	int dd, err;
+
+	dd = hci_open_dev(index);
+	if (dd < 0)
+		return -errno;
+
+	if (pin) {
+		pin_code_reply_cp pr;
+		size_t len = strlen(pin);
+
+		memset(&pr, 0, sizeof(pr));
+		bacpy(&pr.bdaddr, bdaddr);
+		memcpy(pr.pin_code, pin, len);
+		pr.pin_len = len;
+		err = hci_send_cmd(dd, OGF_LINK_CTL, OCF_PIN_CODE_REPLY,
+						PIN_CODE_REPLY_CP_SIZE, &pr);
+	} else
+		err = hci_send_cmd(dd, OGF_LINK_CTL,
+					OCF_PIN_CODE_NEG_REPLY, 6, bdaddr);
+
+	hci_close_dev(dd);
+
+	return err;
+}
+
 static struct btd_adapter_ops hci_ops = {
 	.setup = hciops_setup,
 	.cleanup = hciops_cleanup,
@@ -1142,6 +1169,7 @@ static struct btd_adapter_ops hci_ops = {
 	.disconnect = hciops_disconnect,
 	.remove_bonding = hciops_remove_bonding,
 	.request_authentication = hciops_request_authentication,
+	.pincode_reply = hciops_pincode_reply,
 };
 
 static int hciops_init(void)

src/adapter.c
@@ -3498,3 +3498,9 @@ int btd_adapter_request_authentication(struct btd_adapter *adapter,
 	return adapter_ops->request_authentication(adapter->dev_id,
 							handle, status);
 }
+
+int btd_adapter_pincode_reply(struct btd_adapter *adapter, bdaddr_t *bdaddr,
+							const char *pin)
+{
+	return adapter_ops->pincode_reply(adapter->dev_id, bdaddr, pin);
+}

src/adapter.h
@@ -208,6 +208,7 @@ struct btd_adapter_ops {
 	int (*remove_bonding) (int index, bdaddr_t *bdaddr);
 	int (*request_authentication) (int index, uint16_t handle,
 							uint8_t *status);
+	int (*pincode_reply) (int index, bdaddr_t *bdaddr, const char *pin);
 };
 
 int btd_register_adapter_ops(struct btd_adapter_ops *btd_adapter_ops);
@@ -243,3 +244,6 @@ int btd_adapter_remove_bonding(struct btd_adapter *adapter, bdaddr_t *bdaddr);
 
 int btd_adapter_request_authentication(struct btd_adapter *adapter,
 					uint16_t handle, uint8_t *status);
+
+int btd_adapter_pincode_reply(struct btd_adapter *adapter, bdaddr_t *bdaddr,
+							const char *pin);

commit cdf87d611fb16569a5238ae9d6f33469c5aed7fc

Fix use of uninitialised variable on legacy pairing
Regression caused by e7daece858070d71cecf6ade4f0e3c93272c53ac:

==23899== Use of uninitialised value of size 4
==23899==    at 0x49CD888: _itoa_word (_itoa.c:196)
==23899==    by 0x49D1109: vfprintf (vfprintf.c:1613)
==23899==    by 0x4A7506C: __vsprintf_chk (vsprintf_chk.c:86)
==23899==    by 0x4A74FAC: __sprintf_chk (sprintf_chk.c:33)
==23899==    by 0x4830E08: ba2str (stdio2.h:34)
==23899==    by 0x1496B3: set_pin_length (security.c:514)
==23899==    by 0x168399: pincode_cb (dbus-hci.c:179)
==23899==    by 0x162E0D: pincode_cb (device.c:2135)
==23899==    by 0x15AD55: pincode_reply (agent.c:416)
==23899==    by 0x49467E0: ??? (in /lib/libdbus-1.so.3.5.2)
==23899==    by 0x4934975: ??? (in /lib/libdbus-1.so.3.5.2)
==23899==    by 0x4937B81: dbus_connection_dispatch (in /lib/libdbus-1.so.3.5.2)
==23899==
==23899== Conditional jump or move depends on uninitialised value(s)
==23899==    at 0x49CD893: _itoa_word (_itoa.c:196)
==23899==    by 0x49D1109: vfprintf (vfprintf.c:1613)
==23899==    by 0x4A7506C: __vsprintf_chk (vsprintf_chk.c:86)
==23899==    by 0x4A74FAC: __sprintf_chk (sprintf_chk.c:33)
==23899==    by 0x4830E08: ba2str (stdio2.h:34)
==23899==    by 0x1496B3: set_pin_length (security.c:514)
==23899==    by 0x168399: pincode_cb (dbus-hci.c:179)
==23899==    by 0x162E0D: pincode_cb (device.c:2135)
==23899==    by 0x15AD55: pincode_reply (agent.c:416)
==23899==    by 0x49467E0: ??? (in /lib/libdbus-1.so.3.5.2)
==23899==    by 0x4934975: ??? (in /lib/libdbus-1.so.3.5.2)
==23899==    by 0x4937B81: dbus_connection_dispatch (in /lib/libdbus-1.so.3.5.2)

src/dbus-hci.c
@@ -167,6 +167,7 @@ static void pincode_cb(struct agent *agent, DBusError *derr,
 	bdaddr_t sba, dba;
 	int err;
 
+	adapter_get_address(adapter, &sba);
 	device_get_address(device, &dba);
 
 	err = btd_adapter_pincode_reply(adapter, &dba, derr ? NULL : pincode);