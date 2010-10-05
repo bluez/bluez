bluez/bluez

commit e7daece858070d71cecf6ade4f0e3c93272c53ac

Move PIN code reply to hciops

src/dbus-hci.c
@@ -160,45 +160,23 @@ const char *class_to_icon(uint32_t class)
  *
  *****************************************************************/
 
-static void pincode_cb(struct agent *agent, DBusError *err,
+static void pincode_cb(struct agent *agent, DBusError *derr,
 				const char *pincode, struct btd_device *device)
 {
 	struct btd_adapter *adapter = device_get_adapter(device);
-	pin_code_reply_cp pr;
 	bdaddr_t sba, dba;
-	size_t len;
-	int dev;
-	uint16_t dev_id = adapter_get_dev_id(adapter);
+	int err;
 
-	dev = hci_open_dev(dev_id);
-	if (dev < 0) {
-		error("hci_open_dev(%d): %s (%d)", dev_id,
-				strerror(errno), errno);
+	device_get_address(device, &dba);
+
+	err = btd_adapter_pincode_reply(adapter, &dba, derr ? pincode : NULL);
+	if (err < 0) {
+		error("pincode_reply: %s (%d)", strerror(-err), -err);
 		return;
 	}
 
-	adapter_get_address(adapter, &sba);
-	device_get_address(device, &dba);
-
-	if (err) {
-		hci_send_cmd(dev, OGF_LINK_CTL,
-				OCF_PIN_CODE_NEG_REPLY, 6, &dba);
-		goto done;
-	}
-
-	len = strlen(pincode);
-
-	set_pin_length(&sba, len);
-
-	memset(&pr, 0, sizeof(pr));
-	bacpy(&pr.bdaddr, &dba);
-	memcpy(pr.pin_code, pincode, len);
-	pr.pin_len = len;
-	hci_send_cmd(dev, OGF_LINK_CTL, OCF_PIN_CODE_REPLY,
-						PIN_CODE_REPLY_CP_SIZE, &pr);
-
-done:
-	hci_close_dev(dev);
+	if (derr == NULL)
+		set_pin_length(&sba, strlen(pincode));
 }
 
 int hcid_dbus_request_pin(int dev, bdaddr_t *sba, struct hci_conn_info *ci)

plugins/hciops.c
@@ -1108,6 +1108,33 @@ fail:
 	return err;
 }
 
+static int hciops_pincode_reply(int index, bdaddr_t *bdaddr, const char *pin)
+{
+	int dd, err;
+
+	dd = hci_open_dev(index);
+	if (dd < 0)
+		return -errno;
+
+	if (pin) {
+		pin_code_reply_cp pr;
+		size_t len = strlen(pin);
+
+		memset(&pr, 0, sizeof(pr));
+		bacpy(&pr.bdaddr, bdaddr);
+		memcpy(pr.pin_code, pin, len);
+		pr.pin_len = len;
+		err = hci_send_cmd(dd, OGF_LINK_CTL, OCF_PIN_CODE_REPLY,
+						PIN_CODE_REPLY_CP_SIZE, &pr);
+	} else
+		err = hci_send_cmd(dd, OGF_LINK_CTL,
+					OCF_PIN_CODE_NEG_REPLY, 6, bdaddr);
+
+	hci_close_dev(dd);
+
+	return err;
+}
+
 static struct btd_adapter_ops hci_ops = {
 	.setup = hciops_setup,
 	.cleanup = hciops_cleanup,
@@ -1142,6 +1169,7 @@ static struct btd_adapter_ops hci_ops = {
 	.disconnect = hciops_disconnect,
 	.remove_bonding = hciops_remove_bonding,
 	.request_authentication = hciops_request_authentication,
+	.pincode_reply = hciops_pincode_reply,
 };
 
 static int hciops_init(void)

src/adapter.c
@@ -3498,3 +3498,9 @@ int btd_adapter_request_authentication(struct btd_adapter *adapter,
 	return adapter_ops->request_authentication(adapter->dev_id,
 							handle, status);
 }
+
+int btd_adapter_pincode_reply(struct btd_adapter *adapter, bdaddr_t *bdaddr,
+							const char *pin)
+{
+	return adapter_ops->pincode_reply(adapter->dev_id, bdaddr, pin);
+}

src/adapter.h
@@ -208,6 +208,7 @@ struct btd_adapter_ops {
 	int (*remove_bonding) (int index, bdaddr_t *bdaddr);
 	int (*request_authentication) (int index, uint16_t handle,
 							uint8_t *status);
+	int (*pincode_reply) (int index, bdaddr_t *bdaddr, const char *pin);
 };
 
 int btd_register_adapter_ops(struct btd_adapter_ops *btd_adapter_ops);
@@ -243,3 +244,6 @@ int btd_adapter_remove_bonding(struct btd_adapter *adapter, bdaddr_t *bdaddr);
 
 int btd_adapter_request_authentication(struct btd_adapter *adapter,
 					uint16_t handle, uint8_t *status);
+
+int btd_adapter_pincode_reply(struct btd_adapter *adapter, bdaddr_t *bdaddr,
+							const char *pin);

commit 4a3c238d694d91a3eb2c2140c0998a7051d38d49

Fix regression for Legacy Pairing
When a pincode was entered and no DBus error ocurred, bluetoothd
still responded with Pin Code Request Negative Reply.

The regression was introduced by commit
e7daece858070d71cecf6ade4f0e3c93272c53ac

src/dbus-hci.c
@@ -169,7 +169,7 @@ static void pincode_cb(struct agent *agent, DBusError *derr,
 
 	device_get_address(device, &dba);
 
-	err = btd_adapter_pincode_reply(adapter, &dba, derr ? pincode : NULL);
+	err = btd_adapter_pincode_reply(adapter, &dba, derr ? NULL : pincode);
 	if (err < 0) {
 		error("pincode_reply: %s (%d)", strerror(-err), -err);
 		return;