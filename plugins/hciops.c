06ee7ea71b692212a116e56a4b5047ed82d453b4
Pavan Savoy <pavan_savoy@sify.com>
2011-01-26 16:51:55 +0530

    hciops: remove scan disable on poweroff
    Remove sending of the scan disable command when the device is being
    powered off. This was originally a safe-guard for the non-connectable
    GAP test case in case the HCI driver leaves the controller powered after
    the DEV_DOWN ioctl, but now-days it can be expected that drivers behave
    properly.

--- plugins/hciops.c
+++ plugins/hciops.c
@@ -335,13 +335,8 @@ static int hciops_set_pairable(int index, gboolean pairable)
 static int hciops_power_off(int index)
 {
 	struct dev_info *dev = &devs[index];
-	uint8_t mode = SCAN_DISABLED;
 
 	DBG("hci%d", index);
-
-	if (hci_send_cmd(dev->sk, OGF_HOST_CTL,
-					OCF_WRITE_SCAN_ENABLE, 1, &mode) < 0)
-		return -errno;
 
 	if (ioctl(dev->sk, HCIDEVDOWN, index) < 0 && errno != EALREADY)
 		return -errno;
