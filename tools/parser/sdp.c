c6b73e9b4462de00290cc3faad7a8fd65ee91d9b
Marcel Holtmann <marcel@holtmann.org>
2011-01-27 08:50:11 +0100

    hcidump: Update copyright information

--- tools/parser/sdp.c
+++ tools/parser/sdp.c
@@ -3,7 +3,7 @@
  *  BlueZ - Bluetooth protocol stack for Linux
  *
  *  Copyright (C) 2001-2002  Ricky Yuen <ryuen@qualcomm.com>
- *  Copyright (C) 2003-2010  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2003-2011  Marcel Holtmann <marcel@holtmann.org>
  *
  *
  *  This program is free software; you can redistribute it and/or modify
