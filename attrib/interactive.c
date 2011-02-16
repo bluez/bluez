0439aea80805fd67c729b8d9f72ba00cc6dcbaf8
Claudio Takahasi <claudio.takahasi@openbossa.org>
2011-02-16 13:11:39 -0200

    Fix wrong header include on interactive gatttool
    This wrong include is triggering compiler warning: implicit declaration
    of function 'strdup'.

--- attrib/interactive.c
+++ attrib/interactive.c
@@ -20,7 +20,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <glib.h>
