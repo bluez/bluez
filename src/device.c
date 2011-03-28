bb5eda2bc85b68e9cf3908f39972b476afb6633f
Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
2011-03-28 19:40:48 -0300

    Register Attrib interface after Primary Service discovery

--- src/device.c
+++ src/device.c
@@ -1546,6 +1546,10 @@ static void primary_cb(GSList *services, guint8 status, gpointer user_data)
 	}
 
 	device_probe_drivers(device, uuids);
+
+	/* FIXME: Need the correct psm */
+	attrib_client_register(device, -1, services);
+
 	g_slist_free(uuids);
 
 	create_device_reply(device, req);
