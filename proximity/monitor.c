bluez/bluez

commit ffd21d4027cd91d864857d2d4c3664b3087c5e0a

Add get Immediate Alert for Proximity Monitor
Extends GetProperties method of the Proximity Monitor adding
ImmediateAlertLevel property when Path Loss and/or Find Me is enabled

proximity/monitor.c
@@ -258,6 +258,10 @@ static DBusMessage *get_properties(DBusConnection *conn,
 		dict_append_entry(&dict, "LinkLossAlertLevel",
 				DBUS_TYPE_STRING, &monitor->linklosslevel);
 
+	if (monitor->enabled.findme || monitor->enabled.pathloss)
+		dict_append_entry(&dict, "ImmediateAlertLevel",
+				DBUS_TYPE_STRING, &monitor->linklosslevel);
+
 	dbus_message_iter_close_container(&iter, &dict);
 
 	return reply;

commit cf71a339815704876c979dfc7a03f2e28add3a8d

Fix wrong alert value for ImmediateAlertLevel
GetProperties method is returning wrong value for ImmediateAlertLevel.
Bug introduced by ffd21d4027cd91d864857d2d4c3664b3087c5e0a

proximity/monitor.c
@@ -463,7 +463,7 @@ static DBusMessage *get_properties(DBusConnection *conn,
 
 	if (monitor->enabled.findme || monitor->enabled.pathloss)
 		dict_append_entry(&dict, "ImmediateAlertLevel",
-				DBUS_TYPE_STRING, &monitor->linklosslevel);
+				DBUS_TYPE_STRING, &monitor->immediatelevel);
 
 	if (monitor->enabled.pathloss)
 		dict_append_entry(&dict, "SignalLevel",