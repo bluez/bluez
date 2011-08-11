a21e648cf0421cd9ae25d03a95f86acacc7af071
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2011-08-11 12:53:31 -0300

    avrcp: update copyright

--- audio/control.c
+++ audio/control.c
@@ -4,6 +4,7 @@
  *
  *  Copyright (C) 2006-2010  Nokia Corporation
  *  Copyright (C) 2004-2010  Marcel Holtmann <marcel@holtmann.org>
+ *  Copyright (C) 2011  Texas Instruments, Inc.
  *
  *
  *  This program is free software; you can redistribute it and/or modify
