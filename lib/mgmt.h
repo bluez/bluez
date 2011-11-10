f89f6b68b8fddec39d049a2dbc32188cc6ea8cfc
Johan Hedberg <johan.hedberg@intel.com>
2011-11-10 10:50:44 +0200

    Add struct for start_discovery to mgmt.h

--- lib/mgmt.h
+++ lib/mgmt.h
@@ -216,6 +216,9 @@ struct mgmt_cp_remove_remote_oob_data {
 } __packed;
 
 #define MGMT_OP_START_DISCOVERY		0x001B
+struct mgmt_cp_start_discovery {
+	uint8_t type;
+} __packed;
 
 #define MGMT_OP_STOP_DISCOVERY		0x001C
 
