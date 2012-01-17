c8b8c77b31ec3581aec19017392693ecc36324a5
Johan Hedberg <johan.hedberg@intel.com>
2012-01-17 22:02:03 +0200

    eir: Move EIR data type defines to eir.h

--- src/eir.c
+++ src/eir.c
@@ -39,19 +39,6 @@
 #include "glib-helper.h"
 #include "eir.h"
 
-#define EIR_FLAGS                   0x01  /* flags */
-#define EIR_UUID16_SOME             0x02  /* 16-bit UUID, more available */
-#define EIR_UUID16_ALL              0x03  /* 16-bit UUID, all listed */
-#define EIR_UUID32_SOME             0x04  /* 32-bit UUID, more available */
-#define EIR_UUID32_ALL              0x05  /* 32-bit UUID, all listed */
-#define EIR_UUID128_SOME            0x06  /* 128-bit UUID, more available */
-#define EIR_UUID128_ALL             0x07  /* 128-bit UUID, all listed */
-#define EIR_NAME_SHORT              0x08  /* shortened local name */
-#define EIR_NAME_COMPLETE           0x09  /* complete local name */
-#define EIR_TX_POWER                0x0A  /* transmit power level */
-#define EIR_CLASS_OF_DEV            0x0D  /* Class of Device */
-#define EIR_DEVICE_ID               0x10  /* device ID */
-
 void eir_data_free(struct eir_data *eir)
 {
 	g_slist_free_full(eir->services, g_free);
