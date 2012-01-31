32b758e90f3d49d5473ad7973266822b7e8a4536
Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
2012-01-31 14:10:49 -0300

    device: Fix setting the device as bonded when restoring from the LTK

--- src/device.c
+++ src/device.c
@@ -1025,6 +1025,11 @@ struct btd_device *device_create(DBusConnection *conn,
 		device_set_bonded(device, TRUE);
 	}
 
+	if (device_is_le(device) && has_longtermkeys(&src, &device->bdaddr)) {
+		device_set_paired(device, TRUE);
+		device_set_bonded(device, TRUE);
+	}
+
 	if (read_device_id(srcaddr, address, NULL, &vendor, &product, &version)
 									== 0) {
 		device_set_vendor(device, vendor);
