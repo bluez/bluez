f30c71d3e090f35bd00fe8a806b69715e71df18b
Marcel Holtmann <marcel@holtmann.org>
2012-02-26 18:39:23 -0800

    hciattach: Remove some unneeded includes

--- tools/hciattach_ti.c
+++ tools/hciattach_ti.c
@@ -36,8 +36,6 @@
 #include <sys/types.h>
 #include <sys/param.h>
 #include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/uio.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
--- tools/hciattach_tialt.c
+++ tools/hciattach_tialt.c
@@ -39,8 +39,6 @@
 #include <sys/poll.h>
 #include <sys/param.h>
 #include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/uio.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
