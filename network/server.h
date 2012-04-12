ea22520461e13c180f7e96144c7245a023e8e3f0
Syam Sidhardhan <s.syam@samsung.com>
2012-04-12 20:33:05 +0530

    network: Remove header declared but not defined

--- network/server.h
+++ network/server.h
@@ -25,5 +25,3 @@ int server_init(DBusConnection *conn, gboolean secure);
 void server_exit(void);
 int server_register(struct btd_adapter *adapter);
 int server_unregister(struct btd_adapter *adapter);
-
-int server_find_data(const char *path, const char *pattern);
