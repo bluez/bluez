e61df1600a09334beab122a9580161ae1fabdca5
Johan Hedberg <johan.hedberg@intel.com>
2012-04-18 14:15:50 +0300

    audio: Fix AVDTP Reconfigure Reject message
    The specification describes a proper reject response to
    AVDTP_Reconfigure, so the previously used unknown command response is
    not correct.

--- audio/avdtp.c
+++ audio/avdtp.c
@@ -1633,7 +1633,13 @@ failed:
 static gboolean avdtp_reconf_cmd(struct avdtp *session, uint8_t transaction,
 					struct seid_req *req, int size)
 {
-	return avdtp_unknown_cmd(session, transaction, AVDTP_RECONFIGURE);
+	struct conf_rej rej;
+
+	rej.error = AVDTP_NOT_SUPPORTED_COMMAND;
+	rej.category = 0x00;
+
+	return avdtp_send(session, transaction, AVDTP_MSG_TYPE_REJECT,
+					AVDTP_RECONFIGURE, &rej, sizeof(rej));
 }
 
 static gboolean avdtp_open_cmd(struct avdtp *session, uint8_t transaction,
