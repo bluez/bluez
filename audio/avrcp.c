256fde4f35ad1621c4a493750a46a7b81ff192c9
Michal Labedzki <michal.labedzki@tieto.com>
2012-06-27 15:27:39 +0200

    AVRCP: Convert spaces to tabs
    Fix coding style issue.

--- audio/avrcp.c
+++ audio/avrcp.c
@@ -1117,17 +1117,17 @@ err_metadata:
 
 size_t avrcp_handle_vendor_reject(uint8_t *code, uint8_t *operands)
 {
-    struct avrcp_header *pdu = (void *) operands;
-    uint32_t company_id = get_company_id(pdu->company_id);
+	struct avrcp_header *pdu = (void *) operands;
+	uint32_t company_id = get_company_id(pdu->company_id);
 
-    *code = AVC_CTYPE_REJECTED;
-    pdu->params_len = htons(1);
-    pdu->params[0] = E_INTERNAL;
+	*code = AVC_CTYPE_REJECTED;
+	pdu->params_len = htons(1);
+	pdu->params[0] = E_INTERNAL;
 
-    DBG("rejecting AVRCP PDU 0x%02X, company 0x%06X len 0x%04X",
-            pdu->pdu_id, company_id, pdu->params_len);
+	DBG("rejecting AVRCP PDU 0x%02X, company 0x%06X len 0x%04X",
+				pdu->pdu_id, company_id, pdu->params_len);
 
-    return AVRCP_HEADER_LENGTH + 1;
+	return AVRCP_HEADER_LENGTH + 1;
 }
 
 static struct avrcp_server *find_server(GSList *list, const bdaddr_t *src)
