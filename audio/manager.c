bluez/bluez

commit 813b674bce1aab009e7f2d14d1825f603330563d

Add profile abstraction (replaces btd_device drivers)
This patch removes the btd_device_driver concept and replaces it with
btd_profile. The new construct also contains the necessary parts for
adapter drivers, so btd_adapter_driver is only needed for non-profile
related functionality (most of which is in plugins/*).

The main purpose of this new construct is to facilitate a centralized
connection creation mechanism for profiles, ultimately enabling the
addition of a Device.Connect() method instead of requiring a UI to know
to call e.g. Input.Connect or Audio.Connect.

This feature will also be extended to externally implemented profiles
once the internal implementation gets more stable, such as OBEX (obexd)
and HFP (oFono). The new D-Bus interface will also partially restore
functionality which was previously available through the Serial
interface.

audio/manager.c
@@ -33,6 +33,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <ctype.h>
@@ -126,104 +127,6 @@ static struct audio_adapter *find_adapter(GSList *list,
 	}
 
 	return NULL;
-}
-
-gboolean server_is_enabled(bdaddr_t *src, uint16_t svc)
-{
-	switch (svc) {
-	case HEADSET_SVCLASS_ID:
-		return enabled.headset;
-	case HEADSET_AGW_SVCLASS_ID:
-		return FALSE;
-	case HANDSFREE_SVCLASS_ID:
-		return enabled.headset && enabled.hfp;
-	case HANDSFREE_AGW_SVCLASS_ID:
-		return enabled.gateway;
-	case AUDIO_SINK_SVCLASS_ID:
-		return enabled.sink;
-	case AUDIO_SOURCE_SVCLASS_ID:
-		return enabled.source;
-	case AV_REMOTE_TARGET_SVCLASS_ID:
-	case AV_REMOTE_SVCLASS_ID:
-		return enabled.control;
-	default:
-		return FALSE;
-	}
-}
-
-static void handle_uuid(const char *uuidstr, struct audio_device *device)
-{
-	uuid_t uuid;
-	uint16_t uuid16;
-
-	if (bt_string2uuid(&uuid, uuidstr) < 0) {
-		error("%s not detected as an UUID-128", uuidstr);
-		return;
-	}
-
-	if (!sdp_uuid128_to_uuid(&uuid) && uuid.type != SDP_UUID16) {
-		error("Could not convert %s to a UUID-16", uuidstr);
-		return;
-	}
-
-	uuid16 = uuid.value.uuid16;
-
-	if (!server_is_enabled(&device->src, uuid16)) {
-		DBG("server not enabled for %s (0x%04x)", uuidstr, uuid16);
-		return;
-	}
-
-	switch (uuid16) {
-	case HEADSET_SVCLASS_ID:
-		DBG("Found Headset record");
-		if (device->headset)
-			headset_update(device, uuid16, uuidstr);
-		else
-			device->headset = headset_init(device, uuid16,
-							uuidstr);
-		break;
-	case HEADSET_AGW_SVCLASS_ID:
-		DBG("Found Headset AG record");
-		break;
-	case HANDSFREE_SVCLASS_ID:
-		DBG("Found Handsfree record");
-		if (device->headset)
-			headset_update(device, uuid16, uuidstr);
-		else
-			device->headset = headset_init(device, uuid16,
-								uuidstr);
-		break;
-	case HANDSFREE_AGW_SVCLASS_ID:
-		DBG("Found Handsfree AG record");
-		if (enabled.gateway && (device->gateway == NULL))
-			device->gateway = gateway_init(device);
-		break;
-	case AUDIO_SINK_SVCLASS_ID:
-		DBG("Found Audio Sink");
-		if (device->sink == NULL)
-			device->sink = sink_init(device);
-		break;
-	case AUDIO_SOURCE_SVCLASS_ID:
-		DBG("Found Audio Source");
-		if (device->source == NULL)
-			device->source = source_init(device);
-		break;
-	case AV_REMOTE_SVCLASS_ID:
-	case AV_REMOTE_TARGET_SVCLASS_ID:
-		DBG("Found AV %s", uuid16 == AV_REMOTE_SVCLASS_ID ?
-							"Remote" : "Target");
-		if (device->control)
-			control_update(device->control, uuid16);
-		else
-			device->control = control_init(device, uuid16);
-
-		if (device->sink && sink_is_active(device))
-			avrcp_connect(device);
-		break;
-	default:
-		DBG("Unrecognized UUID: 0x%04X", uuid16);
-		break;
-	}
 }
 
 static sdp_record_t *hsp_ag_record(uint8_t ch)
@@ -769,41 +672,106 @@ failed:
 	return -1;
 }
 
-static int audio_probe(struct btd_device *device, GSList *uuids)
+static struct audio_device *get_audio_dev(struct btd_device *device)
 {
 	struct btd_adapter *adapter = device_get_adapter(device);
 	bdaddr_t src, dst;
-	struct audio_device *audio_dev;
 
 	adapter_get_address(adapter, &src);
 	device_get_address(device, &dst, NULL);
 
-	audio_dev = manager_get_device(&src, &dst, TRUE);
-	if (!audio_dev) {
-		DBG("unable to get a device object");
-		return -1;
-	}
-
-	g_slist_foreach(uuids, (GFunc) handle_uuid, audio_dev);
-
-	return 0;
+	return manager_get_device(&src, &dst, TRUE);
 }
 
 static void audio_remove(struct btd_device *device)
 {
 	struct audio_device *dev;
-	const char *path;
 
-	path = device_get_path(device);
-
-	dev = manager_find_device(path, NULL, NULL, NULL, FALSE);
+	dev = get_audio_dev(device);
 	if (!dev)
 		return;
 
 	devices = g_slist_remove(devices, dev);
-
 	audio_device_unregister(dev);
+}
 
+static int hs_probe(struct btd_device *device, GSList *uuids)
+{
+	struct audio_device *audio_dev;
+
+	audio_dev = get_audio_dev(device);
+	if (!audio_dev) {
+		DBG("unable to get a device object");
+		return -1;
+	}
+
+	if (audio_dev->headset)
+		headset_update(audio_dev, audio_dev->headset, uuids);
+	else
+		audio_dev->headset = headset_init(audio_dev, uuids,
+								enabled.hfp);
+
+	return 0;
+}
+
+static int ag_probe(struct btd_device *device, GSList *uuids)
+{
+	struct audio_device *audio_dev;
+
+	audio_dev = get_audio_dev(device);
+	if (!audio_dev) {
+		DBG("unable to get a device object");
+		return -1;
+	}
+
+	if (!audio_dev->gateway)
+		return -EALREADY;
+
+	audio_dev->gateway = gateway_init(audio_dev);
+
+	return 0;
+}
+
+static int a2dp_probe(struct btd_device *device, GSList *uuids)
+{
+	struct audio_device *audio_dev;
+
+	audio_dev = get_audio_dev(device);
+	if (!audio_dev) {
+		DBG("unable to get a device object");
+		return -1;
+	}
+
+	if (g_slist_find_custom(uuids, A2DP_SINK_UUID, bt_uuid_strcmp) &&
+						audio_dev->sink == NULL)
+		audio_dev->sink = sink_init(audio_dev);
+
+	if (g_slist_find_custom(uuids, A2DP_SOURCE_UUID, bt_uuid_strcmp) &&
+						audio_dev->source == NULL)
+		audio_dev->source = source_init(audio_dev);
+
+	return 0;
+}
+
+static int avrcp_probe(struct btd_device *device, GSList *uuids)
+{
+	struct audio_device *audio_dev;
+
+	audio_dev = get_audio_dev(device);
+	if (!audio_dev) {
+		DBG("unable to get a device object");
+		return -1;
+	}
+
+	if (audio_dev->control)
+		control_update(audio_dev->control, uuids);
+	else
+		audio_dev->control = control_init(audio_dev, uuids);
+
+	if (audio_dev->sink && sink_is_active(audio_dev))
+		avrcp_connect(audio_dev);
+
+	return 0;
 }
 
 static struct audio_adapter *audio_adapter_ref(struct audio_adapter *adp)
@@ -1113,40 +1081,51 @@ static void media_server_remove(struct btd_adapter *adapter)
 	audio_adapter_unref(adp);
 }
 
-static struct btd_device_driver audio_driver = {
-	.name	= "audio",
-	.uuids	= BTD_UUIDS(HSP_HS_UUID, HFP_HS_UUID, HSP_AG_UUID, HFP_AG_UUID,
-			ADVANCED_AUDIO_UUID, A2DP_SOURCE_UUID, A2DP_SINK_UUID,
-			AVRCP_TARGET_UUID, AVRCP_REMOTE_UUID),
-	.probe	= audio_probe,
-	.remove	= audio_remove,
+static struct btd_profile headset_profile = {
+	.name		= "audio-headset",
+
+	.remote_uuids	= BTD_UUIDS(HSP_HS_UUID, HFP_HS_UUID),
+	.device_probe	= hs_probe,
+	.device_remove	= audio_remove,
+
+	.adapter_probe	= headset_server_probe,
+	.adapter_remove = headset_server_remove,
 };
 
-static struct btd_adapter_driver headset_server_driver = {
-	.name	= "audio-headset",
-	.probe	= headset_server_probe,
-	.remove	= headset_server_remove,
+static struct btd_profile gateway_profile = {
+	.name		= "audio-gateway",
+
+	.remote_uuids	= BTD_UUIDS(HSP_AG_UUID, HFP_AG_UUID),
+	.device_probe	= ag_probe,
+	.device_remove	= audio_remove,
+
+	.adapter_probe	= gateway_server_probe,
+	.adapter_remove = gateway_server_remove,
 };
 
-static struct btd_adapter_driver gateway_server_driver = {
-	.name	= "audio-gateway",
-	.probe	= gateway_server_probe,
-	.remove	= gateway_server_remove,
+static struct btd_profile a2dp_profile = {
+	.name		= "audio-a2dp",
+
+	.remote_uuids	= BTD_UUIDS(A2DP_SOURCE_UUID, A2DP_SINK_UUID),
+	.device_probe	= a2dp_probe,
+	.device_remove	= audio_remove,
+
+	.adapter_probe	= a2dp_server_probe,
+	.adapter_remove = a2dp_server_remove,
 };
 
-static struct btd_adapter_driver a2dp_server_driver = {
-	.name	= "audio-a2dp",
-	.probe	= a2dp_server_probe,
-	.remove	= a2dp_server_remove,
+static struct btd_profile avrcp_profile = {
+	.name		= "audio-avrcp",
+
+	.remote_uuids	= BTD_UUIDS(AVRCP_TARGET_UUID, AVRCP_REMOTE_UUID),
+	.device_probe	= avrcp_probe,
+	.device_remove	= audio_remove,
+
+	.adapter_probe	= avrcp_server_probe,
+	.adapter_remove = avrcp_server_remove,
 };
 
-static struct btd_adapter_driver avrcp_server_driver = {
-	.name	= "audio-control",
-	.probe	= avrcp_server_probe,
-	.remove	= avrcp_server_remove,
-};
-
-static struct btd_adapter_driver media_server_driver = {
+static struct btd_adapter_driver media_driver = {
 	.name	= "media",
 	.probe	= media_server_probe,
 	.remove	= media_server_remove,
@@ -1228,22 +1207,20 @@ int audio_manager_init(DBusConnection *conn, GKeyFile *conf,
 		max_connected_headsets = i;
 
 proceed:
-	if (enabled.control)
-		btd_register_adapter_driver(&avrcp_server_driver);
-
-	if (enabled.media)
-		btd_register_adapter_driver(&media_server_driver);
-
 	if (enabled.headset)
-		btd_register_adapter_driver(&headset_server_driver);
+		btd_profile_register(&headset_profile);
 
 	if (enabled.gateway)
-		btd_register_adapter_driver(&gateway_server_driver);
+		btd_profile_register(&gateway_profile);
 
 	if (enabled.source || enabled.sink)
-		btd_register_adapter_driver(&a2dp_server_driver);
+		btd_profile_register(&a2dp_profile);
 
-	btd_register_device_driver(&audio_driver);
+	if (enabled.control)
+		btd_profile_register(&avrcp_profile);
+
+	if (enabled.media)
+		btd_register_adapter_driver(&media_driver);
 
 	*enable_sco = (enabled.gateway || enabled.headset);
 
@@ -1264,22 +1241,20 @@ void audio_manager_exit(void)
 		config = NULL;
 	}
 
-	if (enabled.media)
-		btd_unregister_adapter_driver(&media_server_driver);
-
 	if (enabled.headset)
-		btd_unregister_adapter_driver(&headset_server_driver);
+		btd_profile_unregister(&headset_profile);
 
 	if (enabled.gateway)
-		btd_unregister_adapter_driver(&gateway_server_driver);
+		btd_profile_unregister(&gateway_profile);
 
 	if (enabled.source || enabled.sink)
-		btd_unregister_adapter_driver(&a2dp_server_driver);
+		btd_profile_unregister(&a2dp_profile);
 
 	if (enabled.control)
-		btd_unregister_adapter_driver(&avrcp_server_driver);
+		btd_profile_unregister(&avrcp_profile);
 
-	btd_unregister_device_driver(&audio_driver);
+	if (enabled.media)
+		btd_unregister_adapter_driver(&media_driver);
 }
 
 GSList *manager_find_devices(const char *path,

profiles/deviceinfo/manager.c
@@ -22,6 +22,7 @@
 
 #include <glib.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <bluetooth/uuid.h>
 
 #include "adapter.h"
@@ -62,19 +63,19 @@ static void deviceinfo_driver_remove(struct btd_device *device)
 	deviceinfo_unregister(device);
 }
 
-static struct btd_device_driver deviceinfo_device_driver = {
-	.name	= "deviceinfo-driver",
-	.uuids	= BTD_UUIDS(DEVICE_INFORMATION_UUID),
-	.probe	= deviceinfo_driver_probe,
-	.remove	= deviceinfo_driver_remove
+static struct btd_profile deviceinfo_profile = {
+	.name		= "deviceinfo",
+	.remote_uuids	= BTD_UUIDS(DEVICE_INFORMATION_UUID),
+	.device_probe	= deviceinfo_driver_probe,
+	.device_remove	= deviceinfo_driver_remove
 };
 
 int deviceinfo_manager_init(void)
 {
-	return btd_register_device_driver(&deviceinfo_device_driver);
+	return btd_profile_register(&deviceinfo_profile);
 }
 
 void deviceinfo_manager_exit(void)
 {
-	btd_unregister_device_driver(&deviceinfo_device_driver);
+	btd_profile_unregister(&deviceinfo_profile);
 }

profiles/gatt/manager.c
@@ -22,6 +22,7 @@
 
 #include <glib.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <bluetooth/uuid.h>
 
 #include "adapter.h"
@@ -69,19 +70,19 @@ static void gatt_driver_remove(struct btd_device *device)
 	gas_unregister(device);
 }
 
-static struct btd_device_driver gatt_device_driver = {
-	.name	= "gap-gatt-driver",
-	.uuids	= BTD_UUIDS(GAP_UUID, GATT_UUID),
-	.probe	= gatt_driver_probe,
-	.remove	= gatt_driver_remove
+static struct btd_profile gatt_profile = {
+	.name		= "gap-gatt-profile",
+	.remote_uuids	= BTD_UUIDS(GAP_UUID, GATT_UUID),
+	.device_probe	= gatt_driver_probe,
+	.device_remove	= gatt_driver_remove
 };
 
 int gatt_manager_init(void)
 {
-	return btd_register_device_driver(&gatt_device_driver);
+	return btd_profile_register(&gatt_profile);
 }
 
 void gatt_manager_exit(void)
 {
-	btd_unregister_device_driver(&gatt_device_driver);
+	btd_profile_unregister(&gatt_profile);
 }

profiles/input/manager.c
@@ -26,6 +26,7 @@
 #endif
 
 #include <errno.h>
+#include <stdbool.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/sdp.h>
@@ -139,24 +140,23 @@ static void hid_server_remove(struct btd_adapter *adapter)
 	btd_adapter_unref(adapter);
 }
 
-static struct btd_device_driver input_hid_driver = {
-	.name	= "input-hid",
-	.uuids	= BTD_UUIDS(HID_UUID),
-	.probe	= hid_device_probe,
-	.remove	= hid_device_remove,
+static struct btd_profile input_profile = {
+	.name		= "input-hid",
+	.remote_uuids	= BTD_UUIDS(HID_UUID),
+
+	.device_probe	= hid_device_probe,
+	.device_remove	= hid_device_remove,
+
+	.adapter_probe	= hid_server_probe,
+	.adapter_remove = hid_server_remove,
 };
 
-static struct btd_device_driver input_headset_driver = {
-	.name	= "input-headset",
-	.uuids	= BTD_UUIDS(HSP_HS_UUID),
-	.probe	= headset_probe,
-	.remove	= headset_remove,
-};
+static struct btd_profile input_headset_profile = {
+	.name		= "input-headset",
+	.remote_uuids	= BTD_UUIDS(HSP_HS_UUID),
 
-static struct btd_adapter_driver input_server_driver = {
-	.name   = "input-server",
-	.probe  = hid_server_probe,
-	.remove = hid_server_remove,
+	.device_probe	= headset_probe,
+	.device_remove	= headset_remove,
 };
 
 int input_manager_init(DBusConnection *conn, GKeyFile *config)
@@ -174,22 +174,17 @@ int input_manager_init(DBusConnection *conn, GKeyFile *config)
 
 	connection = dbus_connection_ref(conn);
 
-	btd_register_adapter_driver(&input_server_driver);
-
-	btd_register_device_driver(&input_hid_driver);
-	btd_register_device_driver(&input_headset_driver);
+	btd_profile_register(&input_profile);
+	btd_profile_register(&input_headset_profile);
 
 	return 0;
 }
 
 void input_manager_exit(void)
 {
-	btd_unregister_device_driver(&input_hid_driver);
-	btd_unregister_device_driver(&input_headset_driver);
-
-	btd_unregister_adapter_driver(&input_server_driver);
+	btd_profile_unregister(&input_profile);
+	btd_profile_unregister(&input_headset_profile);
 
 	dbus_connection_unref(connection);
-
 	connection = NULL;
 }

profiles/network/manager.c
@@ -25,6 +25,8 @@
 #include <config.h>
 #endif
 
+#include <stdbool.h>
+
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/bnep.h>
 #include <bluetooth/sdp.h>
@@ -72,7 +74,7 @@ done:
 				conf_security ? "true" : "false");
 }
 
-static int network_probe(struct btd_device *device, GSList *uuids, uint16_t id)
+static int network_probe(struct btd_device *device, GSList *uuids)
 {
 	struct btd_adapter *adapter = device_get_adapter(device);
 	const gchar *path = device_get_path(device);
@@ -83,46 +85,23 @@ static int network_probe(struct btd_device *device, GSList *uuids, uint16_t id)
 	adapter_get_address(adapter, &src);
 	device_get_address(device, &dst, NULL);
 
-	return connection_register(device, path, &src, &dst, id);
+	if (g_slist_find_custom(uuids, PANU_UUID, bt_uuid_strcmp))
+		connection_register(device, path, &src, &dst, BNEP_SVC_PANU);
+	if (g_slist_find_custom(uuids, GN_UUID, bt_uuid_strcmp))
+		connection_register(device, path, &src, &dst, BNEP_SVC_GN);
+	if (g_slist_find_custom(uuids, NAP_UUID, bt_uuid_strcmp))
+		connection_register(device, path, &src, &dst, BNEP_SVC_NAP);
+
+	return 0;
 }
 
-static void network_remove(struct btd_device *device, uint16_t id)
+static void network_remove(struct btd_device *device)
 {
 	const gchar *path = device_get_path(device);
 
 	DBG("path %s", path);
 
-	connection_unregister(path, id);
-}
-
-static int panu_probe(struct btd_device *device, GSList *uuids)
-{
-	return network_probe(device, uuids, BNEP_SVC_PANU);
-}
-
-static void panu_remove(struct btd_device *device)
-{
-	network_remove(device, BNEP_SVC_PANU);
-}
-
-static int gn_probe(struct btd_device *device, GSList *uuids)
-{
-	return network_probe(device, uuids, BNEP_SVC_GN);
-}
-
-static void gn_remove(struct btd_device *device)
-{
-	network_remove(device, BNEP_SVC_GN);
-}
-
-static int nap_probe(struct btd_device *device, GSList *uuids)
-{
-	return network_probe(device, uuids, BNEP_SVC_NAP);
-}
-
-static void nap_remove(struct btd_device *device)
-{
-	network_remove(device, BNEP_SVC_NAP);
+	connection_unregister(path);
 }
 
 static int network_server_probe(struct btd_adapter *adapter)
@@ -143,31 +122,14 @@ static void network_server_remove(struct btd_adapter *adapter)
 	server_unregister(adapter);
 }
 
-static struct btd_device_driver network_panu_driver = {
-	.name	= "network-panu",
-	.uuids	= BTD_UUIDS(PANU_UUID),
-	.probe	= panu_probe,
-	.remove	= panu_remove,
-};
+static struct btd_profile network_profile = {
+	.name		= "network",
+	.remote_uuids	= BTD_UUIDS(PANU_UUID, GN_UUID, NAP_UUID),
+	.device_probe	= network_probe,
+	.device_remove	= network_remove,
 
-static struct btd_device_driver network_gn_driver = {
-	.name	= "network-gn",
-	.uuids	= BTD_UUIDS(GN_UUID),
-	.probe	= gn_probe,
-	.remove	= gn_remove,
-};
-
-static struct btd_device_driver network_nap_driver = {
-	.name	= "network-nap",
-	.uuids	= BTD_UUIDS(NAP_UUID),
-	.probe	= nap_probe,
-	.remove	= nap_remove,
-};
-
-static struct btd_adapter_driver network_server_driver = {
-	.name	= "network-server",
-	.probe	= network_server_probe,
-	.remove	= network_server_remove,
+	.adapter_probe	= network_server_probe,
+	.adapter_remove	= network_server_remove,
 };
 
 int network_manager_init(DBusConnection *conn)
@@ -189,15 +151,10 @@ int network_manager_init(DBusConnection *conn)
 	if (server_init(conn, conf_security) < 0)
 		return -1;
 
-	/* Register network server if it doesn't exist */
-	btd_register_adapter_driver(&network_server_driver);
+	btd_profile_register(&network_profile);
 
 	if (connection_init(conn) < 0)
 		return -1;
-
-	btd_register_device_driver(&network_panu_driver);
-	btd_register_device_driver(&network_gn_driver);
-	btd_register_device_driver(&network_nap_driver);
 
 	connection = dbus_connection_ref(conn);
 
@@ -208,13 +165,9 @@ void network_manager_exit(void)
 {
 	server_exit();
 
-	btd_unregister_device_driver(&network_panu_driver);
-	btd_unregister_device_driver(&network_gn_driver);
-	btd_unregister_device_driver(&network_nap_driver);
-
 	connection_exit();
 
-	btd_unregister_adapter_driver(&network_server_driver);
+	btd_profile_unregister(&network_profile);
 
 	dbus_connection_unref(connection);
 	connection = NULL;

profiles/proximity/manager.c
@@ -26,6 +26,8 @@
 #include <config.h>
 #endif
 
+#include <stdbool.h>
+
 #include <glib.h>
 #include <gdbus.h>
 #include <bluetooth/uuid.h>
@@ -60,6 +62,8 @@ static int attio_device_probe(struct btd_device *device, GSList *uuids)
 	struct gatt_primary *linkloss, *txpower, *immediate;
 	GSList *l, *primaries;
 
+	reporter_device_probe(device);
+
 	primaries = btd_device_get_primaries(device);
 
 	l = g_slist_find_custom(primaries, IMMEDIATE_ALERT_UUID,
@@ -79,19 +83,18 @@ static int attio_device_probe(struct btd_device *device, GSList *uuids)
 static void attio_device_remove(struct btd_device *device)
 {
 	monitor_unregister(connection, device);
+	reporter_device_remove(device);
 }
 
-static struct btd_device_driver monitor_driver = {
-	.name = "Proximity GATT Monitor Driver",
-	.uuids = BTD_UUIDS(IMMEDIATE_ALERT_UUID, LINK_LOSS_UUID, TX_POWER_UUID),
-	.probe = attio_device_probe,
-	.remove = attio_device_remove,
-};
+static struct btd_profile pxp_profile = {
+	.name		= "Proximity GATT Driver",
+	.remote_uuids	= BTD_UUIDS(GATT_UUID, IMMEDIATE_ALERT_UUID,
+						LINK_LOSS_UUID, TX_POWER_UUID),
+	.device_probe	= attio_device_probe,
+	.device_remove	= attio_device_remove,
 
-static struct btd_adapter_driver reporter_server_driver = {
-	.name = "Proximity GATT Reporter Driver",
-	.probe = reporter_init,
-	.remove = reporter_exit,
+	.adapter_probe	= reporter_init,
+	.adapter_remove	= reporter_exit,
 };
 
 static void load_config_file(GKeyFile *config)
@@ -122,29 +125,17 @@ int proximity_manager_init(DBusConnection *conn, GKeyFile *config)
 
 	load_config_file(config);
 
+	ret = btd_profile_register(&pxp_profile);
+	if (ret < 0)
+		return ret;
+
 	connection = dbus_connection_ref(conn);
 
-	ret = btd_register_device_driver(&monitor_driver);
-	if (ret < 0)
-		goto fail_monitor;
-
-	ret = btd_register_adapter_driver(&reporter_server_driver);
-	if (ret < 0)
-		goto fail_reporter;
-
 	return 0;
-
-fail_reporter:
-	btd_unregister_device_driver(&monitor_driver);
-
-fail_monitor:
-	dbus_connection_unref(connection);
-	return ret;
 }
 
 void proximity_manager_exit(void)
 {
-	btd_unregister_device_driver(&monitor_driver);
-	btd_unregister_adapter_driver(&reporter_server_driver);
+	btd_profile_unregister(&pxp_profile);
 	dbus_connection_unref(connection);
 }

profiles/sap/manager.c
@@ -22,8 +22,11 @@
 #include <config.h>
 #endif
 
+#include <stdbool.h>
+
 #include "log.h"
 #include "adapter.h"
+#include "device.h"
 
 #include "manager.h"
 #include "server.h"
@@ -51,10 +54,10 @@ static void sap_server_remove(struct btd_adapter *adapter)
 	sap_server_unregister(path);
 }
 
-static struct btd_adapter_driver sap_server_driver = {
-	.name	= "sap-server",
-	.probe	= sap_server_probe,
-	.remove	= sap_server_remove,
+static struct btd_profile sap_profile = {
+	.name		= "sap-server",
+	.adapter_probe	= sap_server_probe,
+	.adapter_remove	= sap_server_remove,
 };
 
 int sap_manager_init(DBusConnection *conn)
@@ -67,14 +70,14 @@ int sap_manager_init(DBusConnection *conn)
 		return -1;
 	}
 
-	btd_register_adapter_driver(&sap_server_driver);
+	btd_profile_register(&sap_profile);
 
 	return 0;
 }
 
 void sap_manager_exit(void)
 {
-	btd_unregister_adapter_driver(&sap_server_driver);
+	btd_profile_unregister(&sap_profile);
 
 	dbus_connection_unref(connection);
 	connection = NULL;

profiles/thermometer/manager.c
@@ -22,6 +22,7 @@
 
 #include <gdbus.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <bluetooth/uuid.h>
 
 #include "adapter.h"
@@ -64,18 +65,18 @@ static void thermometer_driver_remove(struct btd_device *device)
 	thermometer_unregister(device);
 }
 
-static struct btd_device_driver thermometer_device_driver = {
-	.name	= "thermometer-device-driver",
-	.uuids	= BTD_UUIDS(HEALTH_THERMOMETER_UUID),
-	.probe	= thermometer_driver_probe,
-	.remove	= thermometer_driver_remove
+static struct btd_profile thermometer_profile = {
+	.name		= "thermometer-device-driver",
+	.remote_uuids	= BTD_UUIDS(HEALTH_THERMOMETER_UUID),
+	.device_probe	= thermometer_driver_probe,
+	.device_remove	= thermometer_driver_remove
 };
 
 int thermometer_manager_init(DBusConnection *conn)
 {
 	int ret;
 
-	ret = btd_register_device_driver(&thermometer_device_driver);
+	ret = btd_profile_register(&thermometer_profile);
 	if (ret < 0)
                 return ret;
 
@@ -85,7 +86,7 @@ int thermometer_manager_init(DBusConnection *conn)
 
 void thermometer_manager_exit(void)
 {
-	btd_unregister_device_driver(&thermometer_device_driver);
+	btd_profile_unregister(&thermometer_profile);
 
 	dbus_connection_unref(connection);
 	connection = NULL;

commit bc54a084d10f5a214622600c586e336ce6f7e144

audio: Fix gateway probe always failing
Commit 813b674bce1aab009e7f2d14d1825f603330563d introduced this trivial
bug which causes the failure of the gateway profile probe.

audio/manager.c
@@ -706,7 +706,7 @@ static int ag_probe(struct btd_device *device, GSList *uuids)
 		return -1;
 	}
 
-	if (!audio_dev->gateway)
+	if (audio_dev->gateway)
 		return -EALREADY;
 
 	audio_dev->gateway = gateway_init(audio_dev);