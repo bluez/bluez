67fbf83a6fc0d9a241abdd61be3430a2dd92498b
Szymon Janc <szymon.janc@tieto.com>
2012-10-09 14:11:59 +0200

    storage: Remove not used write_features_info
    This was used in hciops.

--- src/storage.c
+++ src/storage.c
@@ -435,38 +435,6 @@ done:
 	return 0;
 }
 
-int write_features_info(const bdaddr_t *local, const bdaddr_t *peer,
-				unsigned char *page1, unsigned char *page2)
-{
-	char filename[PATH_MAX + 1], addr[18];
-	char str[] = "0000000000000000 0000000000000000";
-	char *old_value;
-	int i;
-
-	ba2str(peer, addr);
-
-	create_filename(filename, PATH_MAX, local, "features");
-	create_file(filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-	old_value = textfile_get(filename, addr);
-
-	if (page1)
-		for (i = 0; i < 8; i++)
-			sprintf(str + (i * 2), "%2.2X", page1[i]);
-	else if (old_value && strlen(old_value) >= 16)
-		strncpy(str, old_value, 16);
-
-	if (page2)
-		for (i = 0; i < 8; i++)
-			sprintf(str + 17 + (i * 2), "%2.2X", page2[i]);
-	else if (old_value && strlen(old_value) >= 33)
-		strncpy(str + 17, old_value + 17, 16);
-
-	free(old_value);
-
-	return textfile_put(filename, addr, str);
-}
-
 static int decode_bytes(const char *str, unsigned char *bytes, size_t len)
 {
 	unsigned int i;
