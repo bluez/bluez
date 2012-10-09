d239258e8d759514af2d390a332661c2d1888ac3
Lucas De Marchi <lucas.de.marchi@gmail.com>
2012-10-09 04:59:54 -0300

    adapter: Fix calling mgmt_set_pairable() with error set

--- src/adapter.c
+++ src/adapter.c
@@ -415,8 +415,12 @@ static DBusMessage *set_pairable(DBusMessage *msg,
 		goto store;
 
 	err = set_mode(adapter, MODE_DISCOVERABLE);
-	if (err < 0 && msg)
-		return btd_error_failed(msg, strerror(-err));
+	if (err < 0) {
+		if (msg != NULL)
+			return btd_error_failed(msg, strerror(-err));
+
+		return NULL;
+	}
 
 store:
 	mgmt_set_pairable(adapter->dev_id, pairable);
