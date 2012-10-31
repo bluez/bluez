bf329be263af755d3fd5beb0591aaf0955f0f883
Johan Hedberg <johan.hedberg@intel.com>
2012-10-31 10:58:42 +0200

    core: Fix coding style in event.h (long lines)

--- src/event.h
+++ src/event.h
@@ -23,16 +23,18 @@
  */
 
 int btd_event_request_pin(bdaddr_t *sba, bdaddr_t *dba, gboolean secure);
-void btd_event_device_found(bdaddr_t *local, bdaddr_t *peer, uint8_t bdaddr_type,
-					int8_t rssi, bool confirm_name,
-					bool legacy, uint8_t *data,
-					uint8_t data_len);
+void btd_event_device_found(bdaddr_t *local, bdaddr_t *peer,
+				uint8_t bdaddr_type, int8_t rssi,
+				bool confirm_name, bool legacy,
+				uint8_t *data, uint8_t data_len);
 void btd_event_remote_name(bdaddr_t *local, bdaddr_t *peer, const char *name);
-void btd_event_conn_complete(bdaddr_t *local, bdaddr_t *peer, uint8_t bdaddr_type,
-						const char *name, uint32_t class);
+void btd_event_conn_complete(bdaddr_t *local, bdaddr_t *peer,
+					uint8_t bdaddr_type, const char *name,
+					uint32_t class);
 void btd_event_conn_failed(bdaddr_t *local, bdaddr_t *peer, uint8_t status);
 void btd_event_disconn_complete(bdaddr_t *local, bdaddr_t *peer);
-void btd_event_simple_pairing_complete(bdaddr_t *local, bdaddr_t *peer, uint8_t status);
+void btd_event_simple_pairing_complete(bdaddr_t *local, bdaddr_t *peer,
+							uint8_t status);
 void btd_event_returned_link_key(bdaddr_t *local, bdaddr_t *peer);
 int btd_event_user_confirm(bdaddr_t *sba, bdaddr_t *dba, uint32_t passkey);
 int btd_event_user_passkey(bdaddr_t *sba, bdaddr_t *dba);
@@ -44,6 +46,6 @@ void btd_event_device_unpaired(bdaddr_t *local, bdaddr_t *peer);
 int btd_event_link_key_notify(bdaddr_t *local, bdaddr_t *peer, uint8_t *key,
 					uint8_t key_type, uint8_t pin_length);
 int btd_event_ltk_notify(bdaddr_t *local, bdaddr_t *peer, uint8_t bdaddr_type,
-					uint8_t *key, uint8_t master,
-					uint8_t authenticated, uint8_t enc_size,
-					uint16_t ediv, uint8_t rand[8]);
+				uint8_t *key, uint8_t master,
+				uint8_t authenticated, uint8_t enc_size,
+				uint16_t ediv, uint8_t rand[8]);
