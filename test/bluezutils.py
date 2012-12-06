f6655d0a884dc7eb2ff5e204ab0b8dede14a39fe
Frédéric Danis <frederic.danis@linux.intel.com>
2012-12-06 17:13:00 -0300

    test: Fix syntax in bluezutils.py
    Traceback (most recent call last):
      File "./simple-agent", line 12, in <module>
        import bluezutils
      File "/home/fdanis/src/bluez/test/bluezutils.py", line 22
        if not pattern or pattern == adapter["Address"] or
                                                         ^
    SyntaxError: invalid syntax

--- test/bluezutils.py
+++ test/bluezutils.py
@@ -19,8 +19,8 @@ def find_adapter_in_objects(objects, pattern=None):
 		adapter = ifaces.get(ADAPTER_INTERFACE)
 		if adapter is None:
 			continue
-		if not pattern or pattern == adapter["Address"] or
-							path.endswith(pattern)):
+		if not pattern or pattern == adapter["Address"] or \
+							path.endswith(pattern):
 			obj = bus.get_object(SERVICE_NAME, path)
 			return dbus.Interface(obj, ADAPTER_INTERFACE)
 	raise Exception("Bluetooth adapter not found")
