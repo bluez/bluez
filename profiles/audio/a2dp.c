a174f7b53d7468a91e04796d80d96d1026b5a46c
Johan Hedberg <johan.hedberg@intel.com>
2012-12-18 11:36:15 +0200

    audio: Remove unused a2dp_get_sep function

--- profiles/audio/a2dp.c
+++ profiles/audio/a2dp.c
@@ -1877,44 +1877,6 @@ gboolean a2dp_sep_get_lock(struct a2dp_sep *sep)
 	return sep->locked;
 }
 
-static int stream_cmp(gconstpointer data, gconstpointer user_data)
-{
-	const struct a2dp_sep *sep = data;
-	const struct avdtp_stream *stream = user_data;
-
-	return (sep->stream != stream);
-}
-
-struct a2dp_sep *a2dp_get_sep(struct avdtp *session,
-				struct avdtp_stream *stream)
-{
-	struct a2dp_server *server;
-	bdaddr_t src, dst;
-	GSList *l;
-
-	avdtp_get_peers(session, &src, &dst);
-
-	for (l = servers; l; l = l->next) {
-		server = l->data;
-
-		if (bacmp(&src, &server->src) == 0)
-			break;
-	}
-
-	if (!l)
-		return NULL;
-
-	l = g_slist_find_custom(server->sources, stream, stream_cmp);
-	if (l)
-		return l->data;
-
-	l = g_slist_find_custom(server->sinks, stream, stream_cmp);
-	if (l)
-		return l->data;
-
-	return NULL;
-}
-
 struct avdtp_stream *a2dp_sep_get_stream(struct a2dp_sep *sep)
 {
 	return sep->stream;
