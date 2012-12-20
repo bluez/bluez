fc09df3af54e156cdbd8f6334382bc6b8e543ace
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2012-12-20 20:34:27 +0200

    obex-client: Rename org.bluez.obex.PhonebookAccess to PhonebookAccess1
    Rename the interface and move it to obexd-api.txt since it now belongs
    to the same daemon.

--- obexd/client/pbap.c
+++ obexd/client/pbap.c
@@ -109,7 +109,7 @@ static const char *filter_list[] = {
 #define FILTER_BIT_MAX	63
 #define FILTER_ALL	0xFFFFFFFFFFFFFFFFULL
 
-#define PBAP_INTERFACE "org.bluez.obex.PhonebookAccess"
+#define PBAP_INTERFACE "org.bluez.obex.PhonebookAccess1"
 #define ERROR_INTERFACE "org.bluez.obex.Error"
 #define PBAP_UUID "0000112f-0000-1000-8000-00805f9b34fb"
 
