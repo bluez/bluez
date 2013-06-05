49cf7c2f63f7b5824c04746447d5ee8a454f74de
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2013-06-05 13:52:45 +0700

    AVRCP: Fix crash if player status is NULL
    Assume status is 'stopped' in case the status is NULL.

--- profiles/audio/avrcp.c
+++ profiles/audio/avrcp.c
@@ -1229,6 +1229,8 @@ static uint8_t player_get_status(struct avrcp_player *player)
 		return AVRCP_PLAY_STATUS_STOPPED;
 
 	value = player->cb->get_status(player->user_data);
+	if (value == NULL)
+		return AVRCP_PLAY_STATUS_STOPPED;
 
 	return play_status_to_val(value);
 }
