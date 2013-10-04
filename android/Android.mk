464cc3f08dab32ebc10fef31fc3014531ccb944d
Frederic Danis <frederic.danis@linux.intel.com>
2013-10-04 14:48:45 +0300

    android-build: Add BlueZ Android daemon
    Define local mapping to glib path, otherwise this has to be inside central
    place in the build repository.

--- android/Android.mk
+++ android/Android.mk
@@ -2,3 +2,28 @@ LOCAL_PATH := $(call my-dir)
 
 # Retrieve BlueZ version from configure.ac file
 BLUEZ_VERSION := $(shell grep ^AC_INIT $(LOCAL_PATH)/../configure.ac | cpp -P -D'AC_INIT(_,v)=v')
+
+# Specify pathmap for glib
+pathmap_INCL += glib:external/bluetooth/glib
+
+#
+# Android BlueZ daemon (bluetoothd)
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	main.c \
+
+LOCAL_C_INCLUDES := \
+	$(call include-path-for, glib) \
+	$(call include-path-for, glib)/glib \
+
+LOCAL_CFLAGS := -DVERSION=\"$(BLUEZ_VERSION)\"
+
+LOCAL_SHARED_LIBRARIES := \
+	libglib \
+
+LOCAL_MODULE := bluetoothd
+
+include $(BUILD_EXECUTABLE)
