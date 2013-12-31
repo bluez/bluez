ae06d36aa188e616e21f9833cbbbc86706c7c9ff
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2013-12-31 15:46:30 +0100

    android/tester: Add BONDED_DEVICES get prop success test case
    This adds BONDED_DEVICES get property success test case.
    
    At this time there are no bonded devices. Clear bonded devices list is
    expected.

--- android/android-tester.c
+++ android/android-tester.c
@@ -891,6 +891,15 @@ static const struct generic_data bluetooth_getprop_uuids_success_test = {
 	.expected_property.len = sizeof(getprop_uuids),
 };
 
+static const struct generic_data bluetooth_getprop_bondeddev_success_test = {
+	.expected_hal_cb.adapter_properties_cb = getprop_success_cb,
+	.expected_cb_count = 1,
+	.expected_adapter_status = BT_STATUS_SUCCESS,
+	.expected_property.type = BT_PROPERTY_ADAPTER_BONDED_DEVICES,
+	.expected_property.val = NULL,
+	.expected_property.len = 0,
+};
+
 static const struct generic_data bluetooth_discovery_start_success_test = {
 	.expected_hal_cb.discovery_state_changed_cb =
 						discovery_start_success_cb,
@@ -1319,6 +1328,19 @@ static void test_getprop_disctimeout_success(const void *test_data)
 }
 
 static void test_getprop_uuids_success(const void *test_data)
+{
+	struct test_data *data = tester_get_data();
+	const struct generic_data *test = data->test_data;
+	const bt_property_t prop = test->expected_property;
+	bt_status_t adapter_status;
+
+	init_test_conditions(data);
+
+	adapter_status = data->if_bluetooth->get_adapter_property(prop.type);
+	check_expected_status(adapter_status);
+}
+
+static void test_getprop_bondeddev_success(const void *test_data)
 {
 	struct test_data *data = tester_get_data();
 	const struct generic_data *test = data->test_data;
@@ -1929,6 +1951,11 @@ int main(int argc, char *argv[])
 					&bluetooth_getprop_uuids_success_test,
 					setup_enabled_adapter,
 					test_getprop_uuids_success, teardown);
+
+	test_bredrle("Bluetooth Get BONDED_DEVICES - Success",
+				&bluetooth_getprop_bondeddev_success_test,
+				setup_enabled_adapter,
+				test_getprop_bondeddev_success, teardown);
 
 	test_bredrle("Bluetooth BREDR Discovery Start - Success",
 				&bluetooth_discovery_start_success_test,
