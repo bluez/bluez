1b95da767808b6361e2db327129261719fa910d1
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2014-01-14 14:54:36 +0100

    android/tester: Add get device COD property success test case
    This adds device COD property success test case.

--- android/android-tester.c
+++ android/android-tester.c
@@ -1363,6 +1363,28 @@ static const struct generic_data bt_dev_getprop_uuids_success_test = {
 	.expected_adapter_status = BT_STATUS_SUCCESS,
 };
 
+static uint32_t remote_getprop_cod_val = 0;
+
+static struct priority_property remote_getprop_cod_props[] = {
+	{
+	.prop.type = BT_PROPERTY_CLASS_OF_DEVICE,
+	.prop.val = &remote_getprop_cod_val,
+	.prop.len = sizeof(remote_getprop_cod_val),
+	},
+};
+
+static const struct generic_data bt_dev_getprop_cod_success_test = {
+	.expected_hal_cb.discovery_state_changed_cb =
+					remote_discovery_state_changed_cb,
+	.expected_hal_cb.device_found_cb = remote_get_property_device_found_cb,
+	.expected_hal_cb.remote_device_properties_cb =
+					remote_test_device_properties_cb,
+	.expected_cb_count = 3,
+	.expected_properties_num = 1,
+	.expected_properties = remote_getprop_cod_props,
+	.expected_adapter_status = BT_STATUS_SUCCESS,
+};
+
 static bt_callbacks_t bt_callbacks = {
 	.size = sizeof(bt_callbacks),
 	.adapter_state_changed_cb = adapter_state_changed_cb,
@@ -1895,6 +1917,15 @@ static void test_dev_getprop_bdname_success(const void *test_data)
 }
 
 static void test_dev_getprop_uuids_success(const void *test_data)
+{
+	struct test_data *data = tester_get_data();
+
+	init_test_conditions(data);
+
+	data->if_bluetooth->start_discovery();
+}
+
+static void test_dev_getprop_cod_success(const void *test_data)
 {
 	struct test_data *data = tester_get_data();
 
@@ -2519,6 +2550,11 @@ int main(int argc, char *argv[])
 				&bt_dev_getprop_uuids_success_test,
 				setup_enabled_adapter,
 				test_dev_getprop_uuids_success, teardown);
+
+	test_bredrle("Bluetooth Device Get COD - Success",
+					&bt_dev_getprop_cod_success_test,
+					setup_enabled_adapter,
+					test_dev_getprop_cod_success, teardown);
 
 	test_bredrle("Socket Init", NULL, setup_socket_interface,
 						test_dummy, teardown);
