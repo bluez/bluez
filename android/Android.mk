14bb94a4dc2bab3befb5d40acfde5415542ecbaa
Marcel Holtmann <marcel@holtmann.org>
2014-01-25 14:18:08 -0800

    android: Compile with ring buffer and HFP framework

--- android/Android.mk
+++ android/Android.mk
@@ -34,6 +34,8 @@ LOCAL_SRC_FILES := \
 	bluez/src/shared/mgmt.c \
 	bluez/src/shared/util.c \
 	bluez/src/shared/queue.c \
+	bluez/src/shared/ringbuf.c \
+	bluez/src/shared/hfp.c \
 	bluez/src/shared/io-glib.c \
 	bluez/src/sdpd-database.c \
 	bluez/src/sdpd-service.c \
