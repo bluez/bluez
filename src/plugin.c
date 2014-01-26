3b3e29cb9c6c248eb85122958aa7206eeee3554f
Marcel Holtmann <marcel@holtmann.org>
2014-01-25 20:10:24 -0800

    core: Use full include path for header files

--- src/plugin.c
+++ src/plugin.c
@@ -35,9 +35,9 @@
 #include <glib.h>
 #include <btio/btio.h>
 
-#include "plugin.h"
-#include "log.h"
-#include "hcid.h"
+#include "src/plugin.h"
+#include "src/log.h"
+#include "src/hcid.h"
 
 static GSList *plugins = NULL;
 
@@ -110,7 +110,7 @@ static gboolean enable_plugin(const char *name, char **cli_enable,
 	return TRUE;
 }
 
-#include "builtin.h"
+#include "src/builtin.h"
 
 gboolean plugin_init(const char *enable, const char *disable)
 {
