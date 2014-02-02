b5321e29db3a4925602473eced82d51b2800f3ce
Marcel Holtmann <marcel@holtmann.org>
2014-02-01 23:06:19 -0800

    tools: Adjust mgmt-tester to new default supported features

--- tools/mgmt-tester.c
+++ tools/mgmt-tester.c
@@ -304,7 +304,7 @@ static void test_condition_complete(struct test_data *data)
 		user->test_data = data; \
 		user->expected_version = 0x06; \
 		user->expected_manufacturer = 0x003f; \
-		user->expected_supported_settings = 0x00000fff; \
+		user->expected_supported_settings = 0x00001fff; \
 		user->initial_settings = 0x00000080; \
 		user->unmet_conditions = 0; \
 		tester_add_full(name, data, \
@@ -323,7 +323,7 @@ static void test_condition_complete(struct test_data *data)
 		user->test_data = data; \
 		user->expected_version = 0x05; \
 		user->expected_manufacturer = 0x003f; \
-		user->expected_supported_settings = 0x000001ff; \
+		user->expected_supported_settings = 0x000011ff; \
 		user->initial_settings = 0x00000080; \
 		user->unmet_conditions = 0; \
 		tester_add_full(name, data, \
@@ -342,7 +342,7 @@ static void test_condition_complete(struct test_data *data)
 		user->test_data = data; \
 		user->expected_version = 0x06; \
 		user->expected_manufacturer = 0x003f; \
-		user->expected_supported_settings = 0x00000611; \
+		user->expected_supported_settings = 0x00001611; \
 		user->initial_settings = 0x00000200; \
 		user->unmet_conditions = 0; \
 		tester_add_full(name, data, \
