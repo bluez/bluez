967acfbac98575fd4d7d0a03ab5cd1030ef3df22
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-02-26 09:41:14 +0100

    android/hal-gatt-api: Add Client Write Descriptor

--- android/hal-msg.h
+++ android/hal-msg.h
@@ -628,6 +628,18 @@ struct hal_cmd_gatt_client_read_descriptor {
 	int32_t auth_req;
 } __attribute__((packed));
 
+#define HAL_OP_GATT_CLIENT_WRITE_DESCRIPTOR	0x0f
+struct hal_cmd_gatt_client_write_descriptor {
+	int32_t conn_id;
+	struct hal_gatt_srvc_id srvc_id;
+	struct hal_gatt_gatt_id char_id;
+	struct hal_gatt_gatt_id descr_id;
+	int32_t write_type;
+	int32_t len;
+	int32_t auth_req;
+	uint8_t value[0];
+} __attribute__((packed));
+
 /* Notifications and confirmations */
 
 #define HAL_POWER_OFF			0x00
