16a85dcd3b5ebaed9539ad0c8bded4961d92a1c2
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2014-02-27 14:28:40 +0100

    android/hal-gatt-api: Add Server Unregister

--- android/hal-msg.h
+++ android/hal-msg.h
@@ -707,6 +707,11 @@ struct hal_cmd_gatt_server_register {
 	uint8_t uuid[16];
 } __attribute__((packed));
 
+#define HAL_OP_GATT_SERVER_UNREGISTER		0x18
+struct hal_cmd_gatt_server_unregister {
+	int32_t server_if;
+} __attribute__((packed));
+
 /* Notifications and confirmations */
 
 #define HAL_POWER_OFF			0x00
