bluez/bluez

commit b278efa5396fe44b110fc2b1dcf7cbff7116057c

android/bluetooth: Send new device events only when needed

android/bluetooth.c
@@ -1323,6 +1323,23 @@ static void update_device(struct device *dev, int8_t rssi,
 					HAL_EV_REMOTE_DEVICE_PROPS, size, buf);
 }
 
+static bool is_new_device(const struct device *dev)
+{
+	if (!dev)
+		return true;
+
+	if (dev->found)
+		return false;
+
+	if (adapter.cur_discovery_type != SCAN_TYPE_DUAL)
+		return false;
+
+	if (dev->bond_state == HAL_BOND_STATE_BONDED)
+		return false;
+
+	return true;
+}
+
 static void update_found_device(const bdaddr_t *bdaddr, uint8_t bdaddr_type,
 					int8_t rssi, bool confirm,
 					const uint8_t *data, uint8_t data_len)
@@ -1352,7 +1369,7 @@ static void update_found_device(const bdaddr_t *bdaddr, uint8_t bdaddr_type,
 	/* Device found event needs to be send also for known device if this is
 	 * new discovery session. Otherwise framework will ignore it.
 	 */
-	if (!dev || !dev->found) {
+	if (is_new_device(dev)) {
 		if (!dev)
 			dev = create_device(bdaddr, bdaddr_type);
 

commit 74a6dc0087efb3a0c6d9b7f5c49cbed365766205

android/gatt: Fix regression introduced by b278efa5
Seems that GattService onScanResult is only looking into remote uuids in
adverisement data. No device object is created in the Android framework,
Therefore we need to send regular device found for BLE devices as well.
Otherwise the Android GATT application will not be aware of remote device
properties like name etc.

android/bluetooth.c
@@ -1331,9 +1331,6 @@ static bool is_new_device(const struct device *dev)
 	if (dev->found)
 		return false;
 
-	if (adapter.cur_discovery_type != SCAN_TYPE_DUAL)
-		return false;
-
 	if (dev->bond_state == HAL_BOND_STATE_BONDED)
 		return false;
 