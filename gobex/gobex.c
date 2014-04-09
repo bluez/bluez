bluez/bluez

commit 35938b779dc8d2498e66e6a6483a36c1f99c2e19

gobex: Fix being able to cancel completed requests
This fixes bogus responses when rsp_func calls g_obex_cancel_req for
responses with final bit set.

gobex/gobex.c
@@ -1098,6 +1098,10 @@ static void handle_response(GObex *obex, GError *err, GObexPacket *rsp)
 
 	p = obex->pending_req;
 
+	/* Reset if final so it can no longer be cancelled */
+	if (final_rsp)
+		obex->pending_req = NULL;
+
 	if (p->cancelled)
 		err = g_error_new(G_OBEX_ERROR, G_OBEX_ERROR_CANCELLED,
 					"The operation was cancelled");
@@ -1116,10 +1120,8 @@ static void handle_response(GObex *obex, GError *err, GObexPacket *rsp)
 	if (p->cancelled)
 		g_error_free(err);
 
-	if (final_rsp) {
+	if (final_rsp)
 		pending_pkt_free(p);
-		obex->pending_req = NULL;
-	}
 
 	if (!disconn && g_queue_get_length(obex->tx_queue) > 0)
 		enable_tx(obex);

commit 021712264c6890667d47b96c45890f5109170fe5

gobex: Fix asserting one more time
This fixes a regression caused by 35938b779dc8d2498e66e6a6483a36c1f99c2e19
where the current pending_req is set to NULL while processing the
response but cause the request to timeout since it is no longer removed
properly.

gobex/gobex.c
@@ -1113,7 +1113,7 @@ static void handle_response(GObex *obex, GError *err, GObexPacket *rsp)
 		p->rsp_func(obex, err, rsp, p->rsp_data);
 
 		/* Check if user callback removed the request */
-		if (p != obex->pending_req)
+		if (!final_rsp && p != obex->pending_req)
 			return;
 	}
 