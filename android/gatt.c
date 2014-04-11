cef3f185307998ff31fca0e50702ba8824771d65
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-04-11 15:15:14 +0200

    android/gatt: Add Server's add_included_service stub

--- android/gatt.c
+++ android/gatt.c
@@ -2899,10 +2899,28 @@ failed:
 
 static void handle_server_add_included_service(const void *buf, uint16_t len)
 {
+	const struct hal_cmd_gatt_server_add_inc_service *cmd = buf;
+	struct gatt_server *server;
+	uint8_t status;
+
 	DBG("");
 
+	server = find_server_by_id(cmd->server_if);
+	if (!server) {
+		error("gatt: server_if=%d not found", cmd->server_if);
+		status = HAL_STATUS_FAILED;
+		goto failed;
+	}
+
+	/* TODO: Add included service to attribute database */
+	DBG("Add included service: server: %d, srvc_hnd: %d, incl_hnd: %d",
+		cmd->server_if, cmd->service_handle, cmd->included_handle);
+
+	status = HAL_STATUS_SUCCESS;
+
+failed:
 	ipc_send_rsp(hal_ipc, HAL_SERVICE_ID_GATT,
-			HAL_OP_GATT_SERVER_ADD_INC_SERVICE, HAL_STATUS_FAILED);
+				HAL_OP_GATT_SERVER_ADD_INC_SERVICE, status);
 }
 
 static void handle_server_add_characteristic(const void *buf, uint16_t len)
