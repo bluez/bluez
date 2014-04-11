f7feed604f7c000bc21a15b4cf284feb57227852
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-04-11 15:15:19 +0200

    android/gatt: Add Server's delete_service stub

--- android/gatt.c
+++ android/gatt.c
@@ -3041,10 +3041,28 @@ failed:
 
 static void handle_server_delete_service(const void *buf, uint16_t len)
 {
+	const struct hal_cmd_gatt_server_delete_service *cmd = buf;
+	struct gatt_server *server;
+	uint8_t status;
+
 	DBG("");
 
+	server = find_server_by_id(cmd->server_if);
+	if (!server) {
+		error("gatt: server_if=%d not found", cmd->server_if);
+		status = HAL_STATUS_FAILED;
+		goto failed;
+	}
+
+	/* TODO: delete service from attribute database */
+	DBG("Delete service: server: %d, srvc_hnd: %d", cmd->server_if,
+							cmd->service_handle);
+
+	status = HAL_STATUS_SUCCESS;
+
+failed:
 	ipc_send_rsp(hal_ipc, HAL_SERVICE_ID_GATT,
-			HAL_OP_GATT_SERVER_DELETE_SERVICE, HAL_STATUS_FAILED);
+				HAL_OP_GATT_SERVER_DELETE_SERVICE, status);
 }
 
 static void handle_server_send_indication(const void *buf, uint16_t len)
