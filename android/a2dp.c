6f15a57645099bc8e49a0ae31f6924efe6958ea3
Andrzej Kaczmarek <andrzej.kaczmarek@tieto.com>
2014-04-17 01:50:33 +0200

    android/a2dp: Prefer master role on incoming connections

--- android/a2dp.c
+++ android/a2dp.c
@@ -1577,6 +1577,7 @@ bool bt_a2dp_register(struct ipc *ipc, const bdaddr_t *addr, uint8_t mode)
 				BT_IO_OPT_SOURCE_BDADDR, &adapter_addr,
 				BT_IO_OPT_PSM, L2CAP_PSM_AVDTP,
 				BT_IO_OPT_SEC_LEVEL, BT_IO_SEC_MEDIUM,
+				BT_IO_OPT_MASTER, true,
 				BT_IO_OPT_INVALID);
 	if (!server) {
 		error("Failed to listen on AVDTP channel: %s", err->message);
