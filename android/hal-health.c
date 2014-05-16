c75e5374f7b1be3671ae93a3134550e3dda9ecee
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-05-16 13:00:42 +0200

    android/hal: Fix not seting mode in health hal
    This fixes the following issue:
    ==8505== Syscall param socketcall.sendmsg(msg.msg_iov[i]) points to
    uninitialised byte(s)
    ==8505==    at 0x534133D: ??? (syscall-template.S:82)
    ==8505==    by 0x7756346: hal_ipc_cmd (hal-ipc.c:359)
    ==8505==    by 0x7750EB2: init (hal-health.c:206)
    ==8505==    by 0x40DF7F: init_p (if-hl.c:86)
    ==8505==    by 0x401961: main (haltest.c:417)
    ==8505==  Address 0x7fefffb81 is on thread 1's stack

--- android/hal-health.c
+++ android/hal-health.c
@@ -202,6 +202,7 @@ static bt_status_t init(bthl_callbacks_t *callbacks)
 				sizeof(ev_handlers)/sizeof(ev_handlers[0]));
 
 	cmd.service_id = HAL_SERVICE_ID_HEALTH;
+	cmd.mode = HAL_MODE_DEFAULT;
 
 	ret = hal_ipc_cmd(HAL_SERVICE_ID_CORE, HAL_OP_REGISTER_MODULE,
 					sizeof(cmd), &cmd, 0, NULL, NULL);
