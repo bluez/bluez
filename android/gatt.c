c2cbfe92abe1a27229aadda1598b9c86e7fe4565
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-05-29 12:34:05 +0200

    android/gatt: Suport read by type request in test command
    'u2' parameter is used as start handle, 'u3' as end handle and uuid as
    attribute type.

--- android/gatt.c
+++ android/gatt.c
@@ -3464,6 +3464,8 @@ static uint8_t handle_test_command_read(bdaddr_t *bdaddr, bt_uuid_t *uuid,
 		length = enc_read_req(u2, pdu, mtu);
 		break;
 	case ATT_OP_READ_BY_TYPE_REQ:
+		length = enc_read_by_type_req(u2, u3, uuid, pdu, mtu);
+		break;
 	case ATT_OP_READ_BLOB_REQ:
 	case ATT_OP_READ_BY_GROUP_REQ:
 	case ATT_OP_READ_MULTI_REQ:
