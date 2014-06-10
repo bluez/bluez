a3bd6579ea0cb04fbe5d86905c49f6e00e6e5a00
Szymon Janc <szymon.janc@tieto.com>
2014-06-10 14:40:38 +0200

    android/hal-a2dp: Use compact syntax for event handlers

--- android/hal-a2dp.c
+++ android/hal-a2dp.c
@@ -53,16 +53,10 @@ static void handle_audio_state(void *buf, uint16_t len)
  * index in table equals to 'opcode - HAL_MINIMUM_EVENT'
  */
 static const struct hal_ipc_handler ev_handlers[] = {
-	{	/* HAL_EV_A2DP_CONN_STATE */
-		.handler = handle_conn_state,
-		.var_len = false,
-		.data_len = sizeof(struct hal_ev_a2dp_conn_state),
-	},
-	{	/* HAL_EV_A2DP_AUDIO_STATE */
-		.handler = handle_audio_state,
-		.var_len = false,
-		.data_len = sizeof(struct hal_ev_a2dp_audio_state),
-	},
+	/* HAL_EV_A2DP_CONN_STATE */
+	{ handle_conn_state, false, sizeof(struct hal_ev_a2dp_conn_state) },
+	/* HAL_EV_A2DP_AUDIO_STATE */
+	{ handle_audio_state, false, sizeof(struct hal_ev_a2dp_audio_state) },
 };
 
 static bt_status_t a2dp_connect(bt_bdaddr_t *bd_addr)
