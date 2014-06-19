054f3bcf0579da0037c3cedb53c89a484049f823
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2014-06-19 13:36:19 +0300

    android/gatt: Fix wrong size argument
    Instead of sizeof of (uint8_t *) pass real length, though for that
    function it doesn't matter at this point.

--- android/gatt.c
+++ android/gatt.c
@@ -5708,7 +5708,7 @@ static void att_handler(const uint8_t *ipdu, uint16_t len, gpointer user_data)
 		 * registered for this indication, event will be send in
 		 * handle_notification
 		 */
-		resp_length = enc_confirmation(opdu, sizeof(opdu));
+		resp_length = enc_confirmation(opdu, length);
 		status = 0;
 		break;
 	case ATT_OP_HANDLE_NOTIFY:
