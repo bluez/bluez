40ba8a3d347ef83669710378830bc49b809b755f
Johan Hedberg <johan.hedberg@intel.com>
2014-07-11 10:38:24 +0300

    profiles/input: Fix usage of PATH_MAX
    PATH_MAX already contains the terminating null so there's no need to +1
    to it. Also, snprintf guarantees a null-terminated string so there's no
    need to explicitly set a terminating character after the call.

--- profiles/input/device.c
+++ profiles/input/device.c
@@ -918,7 +918,7 @@ static int hidp_add_connection(struct input_device *idev)
 	struct hidp_connadd_req *req;
 	sdp_record_t *rec;
 	char src_addr[18], dst_addr[18];
-	char filename[PATH_MAX + 1];
+	char filename[PATH_MAX];
 	GKeyFile *key_file;
 	char handle[11], *str;
 	GError *gerr = NULL;
@@ -935,7 +935,6 @@ static int hidp_add_connection(struct input_device *idev)
 
 	snprintf(filename, PATH_MAX, STORAGEDIR "/%s/cache/%s", src_addr,
 								dst_addr);
-	filename[PATH_MAX] = '\0';
 	sprintf(handle, "0x%8.8X", idev->handle);
 
 	key_file = g_key_file_new();
