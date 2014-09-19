b1ed44f5deace382b2a03529e3d04bb7e7bc9e84
Szymon Janc <szymon.janc@tieto.com>
2014-09-19 16:10:05 +0200

    lib: Fix compilation error with musl
      CC       lib/hci.lo
    In file included from lib/hci.c:39:0:
    /usr/include/sys/poll.h:1:2: error: #warning redirecting incorrect
       #include <sys/poll.h> to <poll.h> [-Werror=cpp]
     #warning redirecting incorrect #include <sys/poll.h> to <poll.h>
      ^
    cc1: all warnings being treated as errors

--- lib/hci.c
+++ lib/hci.c
@@ -33,10 +33,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <poll.h>
 
 #include <sys/param.h>
 #include <sys/uio.h>
-#include <sys/poll.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
