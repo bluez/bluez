7a452b106e9ae9f480566af54f18f82a97d0a8a9
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2014-11-26 12:38:08 +0100

    android/hal-avrcp-ctrl: Fill register module command with max clients
    Register module command must have declared max clients value before
    registering module. Initial value is set to 1.
    
    ==2175== Syscall param sendmsg(msg.msg_iov[1]) points to uninitialised byte(s)
    ==2175==    at 0x534AD5D: ??? (in /usr/lib64/libpthread-2.18.so)
    ==2175==    by 0x6B47F8C: hal_ipc_cmd (hal-ipc.c:369)
    ==2175==    by 0x6B42D42: init (hal-avrcp-ctrl.c:85)
    ==2175==    by 0x404ACF: init_p (if-rc-ctrl.c:56)
    ==2175==    by 0x401975: main (haltest.c:428)
    ==2175==  Address 0xffefffcd2 is on thread 1's stack

--- android/hal-avrcp-ctrl.c
+++ android/hal-avrcp-ctrl.c
@@ -81,6 +81,7 @@ static bt_status_t init(btrc_ctrl_callbacks_t *callbacks)
 
 	cmd.service_id = HAL_SERVICE_ID_AVRCP_CTRL;
 	cmd.mode = HAL_MODE_DEFAULT;
+	cmd.max_clients = 1;
 
 	ret = hal_ipc_cmd(HAL_SERVICE_ID_CORE, HAL_OP_REGISTER_MODULE,
 					sizeof(cmd), &cmd, NULL, NULL, NULL);
