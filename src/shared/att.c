2f560e42703bc82b78c9a9f03787f31675be3927
Jakub Tyszkowski <jakub.tyszkowski@tieto.com>
2014-12-09 09:51:46 +0100

    shared/att: Handle commands under BT_ATT_ALL_REQUESTS
    This is needed for backward compatibility with gattrib.

--- src/shared/att.c
+++ src/shared/att.c
@@ -666,8 +666,10 @@ struct notify_data {
 
 static bool opcode_match(uint8_t opcode, uint8_t test_opcode)
 {
-	if (opcode == BT_ATT_ALL_REQUESTS &&
-				    get_op_type(test_opcode) == ATT_OP_TYPE_REQ)
+	enum att_op_type op_type = get_op_type(test_opcode);
+
+	if (opcode == BT_ATT_ALL_REQUESTS && (op_type == ATT_OP_TYPE_REQ ||
+						op_type == ATT_OP_TYPE_CMD))
 		return true;
 
 	return opcode == test_opcode;
