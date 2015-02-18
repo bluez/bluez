ccea880efd748a62c210702e612264c171505978
Bharat Panda <bharat.panda@samsung.com>
2015-02-18 16:40:06 +0530

    unit/test-gatt: Add TP/GAR/CL/BI-35-C test
    Verify Generic Attribute Profile client behavior when an attempt to use
    LE transport to execute the Read Characteristic Value procedure on a
    characteristic contained within a service defined for use only over
    BR/EDR transport.

--- unit/test-gatt.c
+++ unit/test-gatt.c
@@ -2873,5 +2873,11 @@ int main(int argc, char *argv[])
 			raw_pdu(0x0a, 0x03, 0x00),
 			raw_pdu(0x01, 0x0a, 0x03, 0x00, 0x80));
 
+	define_test_client("/TP/GAR/CL/BI-35-C", test_client, service_db_1,
+			&test_read_12,
+			SERVICE_DATA_1_PDUS,
+			raw_pdu(0x0a, 0x03, 0x00),
+			raw_pdu(0x01, 0x0a, 0x03, 0x00, 0x80));
+
 	return g_test_run();
 }
