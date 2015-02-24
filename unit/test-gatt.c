b303edec36dd9fbcc9a1fc425ba4b33177fff67c
Gowtham Anandha Babu <gowtham.ab@samsung.com>
2015-02-24 16:47:38 +0530

    unit/test-gatt: Add TP/GAW/CL/BI-04-C test
    Verify Generic Attribute Profile client behavior when the
    Write Characteristic Value procedure fails due to insufficient
    authorization.

--- unit/test-gatt.c
+++ unit/test-gatt.c
@@ -804,6 +804,14 @@ static const struct test_step test_write_3 = {
 	.length = 0x03
 };
 
+static const struct test_step test_write_4 = {
+	.handle = 0x0007,
+	.func = test_write,
+	.expected_att_ecode = 0x08,
+	.value = write_data_1,
+	.length = 0x03
+};
+
 static void att_write_cb(struct gatt_db_attribute *att, int err,
 								void *user_data)
 {
@@ -2908,6 +2916,12 @@ int main(int argc, char *argv[])
 			SERVICE_DATA_1_PDUS,
 			raw_pdu(0x12, 0x07, 0x00, 0x01, 0x02, 0x03),
 			raw_pdu(0x01, 0x12, 0x07, 0x00, 0x03));
+
+	define_test_client("/TP/GAW/CL/BI-04-C", test_client, service_db_1,
+			&test_write_4,
+			SERVICE_DATA_1_PDUS,
+			raw_pdu(0x12, 0x07, 0x00, 0x01, 0x02, 0x03),
+			raw_pdu(0x01, 0x12, 0x07, 0x00, 0x08));
 
 	return tester_run();
 }
