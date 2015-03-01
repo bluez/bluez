00fa3028519196f247cef9144d6f0e2e12aaa841
Marcel Holtmann <marcel@holtmann.org>
2015-03-01 06:27:17 +0100

    tools: Use local libbluetooth includes

--- tools/csr.c
+++ tools/csr.c
@@ -35,9 +35,9 @@
 #include <sys/mman.h>
 #include <sys/socket.h>
 
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include "lib/bluetooth.h"
+#include "lib/hci.h"
+#include "lib/hci_lib.h"
 
 #include "csr.h"
 
--- tools/hcitool.c
+++ tools/hcitool.c
@@ -40,9 +40,9 @@
 #include <sys/socket.h>
 #include <signal.h>
 
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/hci.h>
-#include <bluetooth/hci_lib.h>
+#include "lib/bluetooth.h"
+#include "lib/hci.h"
+#include "lib/hci_lib.h"
 
 #include "src/oui.h"
 
