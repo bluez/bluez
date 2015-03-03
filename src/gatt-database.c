3361ea2cc366bd37d88187394bb8031267010523
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-03-03 11:12:56 +0200

    core/gatt-database: Unregister interface on destroy
    This makes sure GattManager interface is unregistered on
    btd_gatt_database_destroy.

--- src/gatt-database.c
+++ src/gatt-database.c
@@ -1695,6 +1695,10 @@ void btd_gatt_database_destroy(struct btd_gatt_database *database)
 	if (!database)
 		return;
 
+	g_dbus_unregister_interface(btd_get_dbus_connection(),
+					adapter_get_path(database->adapter),
+					GATT_MANAGER_IFACE);
+
 	gatt_database_free(database);
 }
 
