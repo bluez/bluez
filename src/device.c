1e4a95b2d2b2951fad8b15e0545cfe3503b4ae50
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-03-17 10:43:41 +0200

    core/device: Fix btd_device_set_temporary
    In case the device is set as not temporary it should trigger
    store_device_info but the function checks if temporary flag is set but
    since currently the code only set the flag after the call nothing really
    happens.

--- src/device.c
+++ src/device.c
@@ -4474,17 +4474,19 @@ void btd_device_set_temporary(struct btd_device *device, gboolean temporary)
 
 	DBG("temporary %d", temporary);
 
+	device->temporary = temporary;
+
 	if (temporary) {
 		if (device->bredr)
 			adapter_whitelist_remove(device->adapter, device);
 		adapter_connect_list_remove(device->adapter, device);
-	} else {
-		if (device->bredr)
-			adapter_whitelist_add(device->adapter, device);
-		store_device_info(device);
+		return;
 	}
 
-	device->temporary = temporary;
+	if (device->bredr)
+		adapter_whitelist_add(device->adapter, device);
+
+	store_device_info(device);
 }
 
 void btd_device_set_trusted(struct btd_device *device, gboolean trusted)
