e6542cd31da492e2e75691bde3547cfb3ea137d4
Bharat Panda <bharat.panda@samsung.com>
2015-04-09 16:41:31 +0530

    unit/test-gatt: Add TP/GAW/CL/BI-31-C test
    Verify Generic Attribute Profile client behavior when the Write
    Long Characteristic Descriptor procedure fails due to insufficient
    encryption key size.

--- unit/test-gatt.c
+++ unit/test-gatt.c
@@ -1130,6 +1130,14 @@ static const struct test_step test_long_write_11 = {
 	.length = 0x03
 };
 
+static const struct test_step test_long_write_12 = {
+	.handle = 0x0008,
+	.func = test_long_write,
+	.expected_att_ecode = 0x0c,
+	.value = write_data_1,
+	.length = 0x03
+};
+
 static void att_write_cb(struct gatt_db_attribute *att, int err,
 								void *user_data)
 {
@@ -4157,6 +4165,14 @@ int main(int argc, char *argv[])
 			SERVICE_DATA_1_PDUS,
 			raw_pdu(0x16, 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03),
 			raw_pdu(0x01, 0x16, 0x08, 0x00, 0x05),
+			raw_pdu(0x18, 0x00),
+			raw_pdu(0x19));
+
+	define_test_client("/TP/GAW/CL/BI-31-C", test_client, service_db_1,
+			&test_long_write_12,
+			SERVICE_DATA_1_PDUS,
+			raw_pdu(0x16, 0x08, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03),
+			raw_pdu(0x01, 0x16, 0x08, 0x00, 0x0c),
 			raw_pdu(0x18, 0x00),
 			raw_pdu(0x19));
 
