19bc2d933585749a8230006445779f28d58a944d
Gowtham Anandha Babu <gowtham.ab@samsung.com>
2015-04-16 18:18:39 +0530

    unit/gatt: Add TP/GAW/CL/BI-18-C test
    Verify Generic Attribute Profile client behavior when the Characteristic
    Value Reliable Write procedure fails due to insufficient authentication.

--- unit/test-gatt.c
+++ unit/test-gatt.c
@@ -1193,6 +1193,14 @@ static const struct test_step test_reliable_write_4 = {
 	.length = 0x03
 };
 
+static const struct test_step test_reliable_write_5 = {
+	.handle = 0x0007,
+	.func = test_reliable_write,
+	.expected_att_ecode = 0x05,
+	.value = write_data_1,
+	.length = 0x03
+};
+
 static void att_write_cb(struct gatt_db_attribute *att, int err,
 								void *user_data)
 {
@@ -3864,6 +3872,14 @@ int main(int argc, char *argv[])
 			SERVICE_DATA_1_PDUS,
 			raw_pdu(0x16, 0x07, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03),
 			raw_pdu(0x01, 0x16, 0x07, 0x00, 0x08),
+			raw_pdu(0x18, 0x00),
+			raw_pdu(0x19));
+
+	define_test_client("/TP/GAW/CL/BI-18-C", test_client, service_db_1,
+			&test_reliable_write_5,
+			SERVICE_DATA_1_PDUS,
+			raw_pdu(0x16, 0x07, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03),
+			raw_pdu(0x01, 0x16, 0x07, 0x00, 0x05),
 			raw_pdu(0x18, 0x00),
 			raw_pdu(0x19));
 
