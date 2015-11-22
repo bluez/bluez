a71753eba32fd2c201734088cf33ebc80ed23b7c
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2015-11-22 21:20:24 +0100

    monitor/avdtp: Decode AVDTP_ABORT
    > ACL Data RX: Handle 256 flags 0x02 dlen 7
          Channel: 66 len 3 [PSM 25 mode 0] {chan 2}
          AVDTP: Abort (0x0a) Command (0x00) type 0x00 label 3 nosp 0
            ACP SEID: 3
    < ACL Data TX: Handle 256 flags 0x00 dlen 6
          Channel: 258 len 2 [PSM 25 mode 0] {chan 2}
          AVDTP: Abort (0x0a) Response Accept (0x02) type 0x00 label 3 nosp 0

--- monitor/avdtp.c
+++ monitor/avdtp.c
@@ -496,6 +496,27 @@ static bool avdtp_suspend(struct avdtp_frame *avdtp_frame)
 	return false;
 }
 
+static bool avdtp_abort(struct avdtp_frame *avdtp_frame)
+{
+	struct l2cap_frame *frame = &avdtp_frame->l2cap_frame;
+	uint8_t type = avdtp_frame->hdr & 0x03;
+	uint8_t seid;
+
+	switch (type) {
+	case AVDTP_MSG_TYPE_COMMAND:
+		if (!l2cap_frame_get_u8(frame, &seid))
+			return false;
+
+		print_field("ACP SEID: %d", seid >> 2);
+
+		return true;
+	case AVDTP_MSG_TYPE_RESPONSE_ACCEPT:
+		return true;
+	}
+
+	return false;
+}
+
 static bool avdtp_signalling_packet(struct avdtp_frame *avdtp_frame)
 {
 	struct l2cap_frame *frame = &avdtp_frame->l2cap_frame;
@@ -569,6 +590,8 @@ static bool avdtp_signalling_packet(struct avdtp_frame *avdtp_frame)
 		return avdtp_close(avdtp_frame);
 	case AVDTP_SUSPEND:
 		return avdtp_suspend(avdtp_frame);
+	case AVDTP_ABORT:
+		return avdtp_abort(avdtp_frame);
 	}
 
 	packet_hexdump(frame->data, frame->size);
