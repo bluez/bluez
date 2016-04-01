cc235a8d528b36cad2cf9fe6517d54711613331a
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-04-01 11:07:15 +0300

    audio/avrcp: Fix registering to player events
    If controller does not have a player skip registering to events which
    requires a player.

--- profiles/audio/avrcp.c
+++ profiles/audio/avrcp.c
@@ -3647,8 +3647,9 @@ static gboolean avrcp_get_capabilities_resp(struct avctp *conn,
 		case AVRCP_EVENT_ADDRESSED_PLAYER_CHANGED:
 		case AVRCP_EVENT_UIDS_CHANGED:
 		case AVRCP_EVENT_AVAILABLE_PLAYERS_CHANGED:
-			/* These events above are controller specific */
-			if (!session->controller)
+			/* These events above requires a player */
+			if (!session->controller ||
+						!session->controller->player)
 				break;
 		case AVRCP_EVENT_VOLUME_CHANGED:
 			avrcp_register_notification(session, event);
