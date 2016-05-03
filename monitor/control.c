868a9f11a77a49dd3384d03b8f12c18fc5b01683
Johan Hedberg <johan.hedberg@intel.com>
2016-05-03 23:15:33 +0300

    monitor: Add support for writing to BTSnoop file from TTY input

--- monitor/control.c
+++ monitor/control.c
@@ -1290,8 +1290,10 @@ static void tty_callback(int fd, uint32_t events, void *user_data)
 		opcode = le16_to_cpu(hdr->opcode);
 		pktlen = data_len - 4 - hdr->hdr_len;
 
+		btsnoop_write_hci(btsnoop_file, tv, 0, opcode,
+					hdr->ext_hdr + hdr->hdr_len, pktlen);
 		packet_monitor(tv, NULL, 0, opcode,
-				hdr->ext_hdr + hdr->hdr_len, pktlen);
+					hdr->ext_hdr + hdr->hdr_len, pktlen);
 
 		data->offset -= 2 + data_len;
 
