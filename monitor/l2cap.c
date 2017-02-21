8673a556e94fdebd41e93280cdceb705175d1c3b
Szymon Janc <szymon.janc@codecoup.pl>
2017-02-21 14:22:01 +0100

    monitor: Fix LE Connection Response error decoding
    "Connection refused - Source CID already allocated" is 0x000a not 0x0010
    
    > ACL Data RX: Handle 76 flags 0x02 dlen 18
          LE L2CAP: LE Connection Response (0x15) ident 7 len 10
            Destination CID: 0
            MTU: 0
            MPS: 0
            Credits: 0
            Result: Connection refused - Source CID already allocated (0x000a)

--- monitor/l2cap.c
+++ monitor/l2cap.c
@@ -573,7 +573,7 @@ static void print_le_conn_result(uint16_t result)
 	case 0x0009:
 		str = "Connection refused - Invalid Source CID";
 		break;
-	case 0x0010:
+	case 0x000a:
 		str = "Connection refused - Source CID already allocated";
 		break;
 	default:
