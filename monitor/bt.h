fbf4a060e82af94e7e5087c041b6105fde2530c6
Marcel Holtmann <marcel@holtmann.org>
2017-05-01 22:19:28 -0700

    monitor: Fix parameter indentation for PHY commands

--- monitor/bt.h
+++ monitor/bt.h
@@ -2139,25 +2139,25 @@ struct bt_hci_cmd_le_read_phy {
 	uint16_t handle;
 } __attribute__((packed));
 struct bt_hci_rsp_le_read_phy {
-	uint8_t status;
+	uint8_t  status;
 	uint16_t handle;
-	uint8_t tx_phy;
-	uint8_t rx_phy;
+	uint8_t  tx_phy;
+	uint8_t  rx_phy;
 } __attribute__((packed));
 
 #define BT_HCI_CMD_LE_SET_DEFAULT_PHY		0x2031
 struct bt_hci_cmd_le_set_default_phy {
-	uint8_t all_phys;
-	uint8_t tx_phys;
-	uint8_t rx_phys;
+	uint8_t  all_phys;
+	uint8_t  tx_phys;
+	uint8_t  rx_phys;
 } __attribute__((packed));
 
 #define BT_HCI_CMD_LE_SET_PHY			0x2032
 struct bt_hci_cmd_le_set_phy {
 	uint16_t handle;
-	uint8_t all_phys;
-	uint8_t tx_phys;
-	uint8_t rx_phys;
+	uint8_t  all_phys;
+	uint8_t  tx_phys;
+	uint8_t  rx_phys;
 	uint16_t phy_opts;
 } __attribute__((packed));
 
