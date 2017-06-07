ffc08888573c26628b304fae1d6614a1dec1c412
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2017-06-07 10:39:44 +0200

    monitor/avdtp: Correct bit mask for TSEP
    According to AVDP specification 1.3 (8.6.2 Stream End Point Discovery
    Response), TSEP bit is on 3rd position of 3rd frame byte.

--- monitor/avdtp.c
+++ monitor/avdtp.c
@@ -368,7 +368,7 @@ static bool avdtp_discover(struct avdtp_frame *avdtp_frame)
 			print_field("%*cMedia Type: %s (0x%02x)", 2, ' ',
 					mediatype2str(info >> 4), info >> 4);
 			print_field("%*cSEP Type: %s (0x%02x)", 2, ' ',
-						info & 0x04 ? "SNK" : "SRC",
+						info & 0x08 ? "SNK" : "SRC",
 						(info >> 3) & 0x01);
 			print_field("%*cIn use: %s", 2, ' ',
 						seid & 0x02 ? "Yes" : "No");
