76255f732d68aef2b90d36d9c7be51a9e1739ce7
Andy Duan <fugang.duan@nxp.com>
2017-07-20 16:14:59 +0300

    hciattach: fix the delay timer for bcm43xx firmware download
    From the log in .bcm43xx_load_firmware():
            /* Wait 50ms to let the firmware placed in download mode */
            nanosleep(&tm_mode, NULL);
    
    But timespec tm_mode is real is 50us. Correct the delayed timer count.

--- tools/hciattach_bcm43xx.c
+++ tools/hciattach_bcm43xx.c
@@ -228,8 +228,8 @@ static int bcm43xx_set_speed(int fd, struct termios *ti, uint32_t speed)
 static int bcm43xx_load_firmware(int fd, const char *fw)
 {
 	unsigned char cmd[] = { HCI_COMMAND_PKT, 0x2e, 0xfc, 0x00 };
-	struct timespec tm_mode = { 0, 50000 };
-	struct timespec tm_ready = { 0, 2000000 };
+	struct timespec tm_mode = { 0, 50000000 };
+	struct timespec tm_ready = { 0, 200000000 };
 	unsigned char resp[CC_MIN_SIZE];
 	unsigned char tx_buf[1024];
 	int len, fd_fw, n;
