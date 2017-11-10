bluez/bluez

commit 7ece89b0b6f1ef8b4d4ca132a5163c08a14c41e2

avdtp: Fix not disconnecting
avdtp_unref would actually wait 1 second before disconnecting which may
be enough for the remote device to start a new stream when it should
not be able to that anymore.

profiles/audio/avdtp.c
@@ -1154,9 +1154,12 @@ static void set_disconnect_timer(struct avdtp *session)
 	if (session->dc_timer)
 		remove_disconnect_timer(session);
 
-	session->dc_timer = g_timeout_add_seconds(DISCONNECT_TIMEOUT,
-						disconnect_timeout,
-						session);
+	if (!session->stream_setup)
+		session->dc_timer = g_idle_add(disconnect_timeout, session);
+	else
+		session->dc_timer = g_timeout_add_seconds(DISCONNECT_TIMEOUT,
+							disconnect_timeout,
+							session);
 }
 
 void avdtp_unref(struct avdtp *session)

commit 9bb331f2d5e63b2466647d11f5fc8b6685649cab

avdtp: Fix disconnecting before stream setup completes
This is a regression introduced by
7ece89b0b6f1ef8b4d4ca132a5163c08a14c41e2 which cause the disconnection
to happen immediatelly when the last reference is droppped but since the
stream setup may have been started by the remove end there is a chance
a stream is currently ongoing in which case the previous behavior of
not disconnecting immediatelly is required in order to give time for the
end to complete any pending request.

profiles/audio/avdtp.c
@@ -1002,6 +1002,7 @@ static void avdtp_sep_set_state(struct avdtp *session,
 			handle_unanswered_req(session, stream);
 		/* Remove pending commands for this stream from the queue */
 		cleanup_queue(session, stream);
+		session->streams = g_slist_remove(session->streams, stream);
 		break;
 	default:
 		break;
@@ -1014,11 +1015,8 @@ static void avdtp_sep_set_state(struct avdtp *session,
 		cb->cb(stream, old_state, state, err_ptr, cb->user_data);
 	}
 
-	if (state == AVDTP_STATE_IDLE &&
-				g_slist_find(session->streams, stream)) {
-		session->streams = g_slist_remove(session->streams, stream);
+	if (state == AVDTP_STATE_IDLE)
 		stream_free(stream);
-	}
 }
 
 static void finalize_discovery(struct avdtp *session, int err)
@@ -1154,7 +1152,7 @@ static void set_disconnect_timer(struct avdtp *session)
 	if (session->dc_timer)
 		remove_disconnect_timer(session);
 
-	if (!session->stream_setup)
+	if (!session->stream_setup && !session->streams)
 		session->dc_timer = g_idle_add(disconnect_timeout, session);
 	else
 		session->dc_timer = g_timeout_add_seconds(DISCONNECT_TIMEOUT,