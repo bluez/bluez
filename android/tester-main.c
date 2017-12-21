4c948b8e9155097edf0b2bad8c8921fcc5bb456b
Szymon Janc <szymon.janc@codecoup.pl>
2017-12-21 09:43:39 +0100

    android/tester: Fix remote advertising data
    Advertising Data doesn't have terminator field like EIR. This was
    causing error logs from kernel when data length was corrected.

--- android/tester-main.c
+++ android/tester-main.c
@@ -2722,12 +2722,11 @@ void emu_setup_powered_remote_action(void)
 
 	if ((data->hciemu_type == HCIEMU_TYPE_LE) ||
 				(data->hciemu_type == HCIEMU_TYPE_BREDRLE)) {
-		uint8_t adv[4];
+		uint8_t adv[3];
 
 		adv[0] = 0x02;	/* Field length */
 		adv[1] = 0x01;	/* Flags */
 		adv[2] = 0x02;	/* Flags value */
-		adv[3] = 0x00;	/* Field terminator */
 
 		bthost_set_adv_data(bthost, adv, sizeof(adv));
 		bthost_set_adv_enable(bthost, 0x01);
