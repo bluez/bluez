5cd995a95bb5c8d9e99840414385214f0f9c8fc8
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2018-12-18 12:02:22 -0300

    gdbus: Split validation of object path and interface
    This splits the validation of object and interface so and error is
    properly printed for each of those.

--- gdbus/object.c
+++ gdbus/object.c
@@ -1346,9 +1346,15 @@ gboolean g_dbus_register_interface(DBusConnection *connection,
 {
 	struct generic_data *data;
 
-	if (!dbus_validate_path(path, NULL) ||
-				!dbus_validate_interface(name, NULL))
+	if (!dbus_validate_path(path, NULL)) {
+		error("Invalid object path: %s", path);
 		return FALSE;
+	}
+
+	if (!dbus_validate_interface(name, NULL)) {
+		error("Invalid interface: %s", name);
+		return FALSE;
+	}
 
 	data = object_path_ref(connection, path);
 	if (data == NULL)
