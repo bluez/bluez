61244c0c2f5694a344d76d5feb722e82327cd9cf
Pali Rohár <pali.rohar@gmail.com>
2019-01-29 18:12:07 +0100

    a2dp-codecs: Fix SBC_MAX_BITPOOL and add SBC quality modes
    According to A2DP specification; section SBC; Codec Specific Information
    Elements; Minimum / Maximum Bitpool Value, range for Bitpool value is from
    2 to 250.
    
    A2DP specification also defines bitpool values for two SBC modes: Middle
    Quality and High Quality. They depends on channel mode and frequency. So
    add definitions for them into a2dp-codecs file too.
    
    File android/hal-audio-sbc.c was updated to use High Quality mode for
    chosen frequency.

--- android/hal-audio-sbc.c
+++ android/hal-audio-sbc.c
@@ -92,7 +92,7 @@ static const a2dp_sbc_t sbc_presets[] = {
 		.block_length = SBC_BLOCK_LENGTH_4 | SBC_BLOCK_LENGTH_8 |
 				SBC_BLOCK_LENGTH_12 | SBC_BLOCK_LENGTH_16,
 		.min_bitpool = SBC_MIN_BITPOOL,
-		.max_bitpool = SBC_MAX_BITPOOL
+		.max_bitpool = SBC_BITPOOL_HQ_JOINT_STEREO_44100,
 	},
 	{
 		.frequency = SBC_SAMPLING_FREQ_44100,
@@ -101,7 +101,7 @@ static const a2dp_sbc_t sbc_presets[] = {
 		.allocation_method = SBC_ALLOCATION_LOUDNESS,
 		.block_length = SBC_BLOCK_LENGTH_16,
 		.min_bitpool = SBC_MIN_BITPOOL,
-		.max_bitpool = SBC_MAX_BITPOOL
+		.max_bitpool = SBC_BITPOOL_HQ_JOINT_STEREO_44100,
 	},
 	{
 		.frequency = SBC_SAMPLING_FREQ_48000,
@@ -110,7 +110,7 @@ static const a2dp_sbc_t sbc_presets[] = {
 		.allocation_method = SBC_ALLOCATION_LOUDNESS,
 		.block_length = SBC_BLOCK_LENGTH_16,
 		.min_bitpool = SBC_MIN_BITPOOL,
-		.max_bitpool = SBC_MAX_BITPOOL
+		.max_bitpool = SBC_BITPOOL_HQ_JOINT_STEREO_48000,
 	},
 };
 
