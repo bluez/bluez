af3f7f1b099790b899e14f29265f8b083b29e3f6
Inga Stotland <inga.stotland@intel.com>
2019-05-15 12:00:18 -0700

    mesh: Update copyright dates

--- mesh/storage.h
+++ mesh/storage.h
@@ -2,7 +2,7 @@
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
- *  Copyright (C) 2017-2918  Intel Corporation. All rights reserved.
+ *  Copyright (C) 2017-2019  Intel Corporation. All rights reserved.
  *
  *
  *  This library is free software; you can redistribute it and/or
