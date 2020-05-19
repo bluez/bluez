c924331486621e33a54f83064f2763dc988a9cb6
Miao-chen Chou <mcchou@chromium.org>
2020-05-18 20:10:00 -0700

    lib: Add definitions for advertisement monitor features
    This adds the following command opcodes, event codes and the corresponding
    structures.
    - MGMT_OP_READ_ADV_MONITOR_FEATURES
    - MGMT_OP_ADD_ADV_PATTERNS_MONITOR
    - MGMT_OP_REMOVE_ADV_MONITOR
    - MGMT_EV_ADV_MONITOR_ADDED
    - MGMT_EV_ADV_MONITOR_REMOVED

--- lib/mgmt.h
+++ lib/mgmt.h
@@ -646,6 +646,42 @@ struct mgmt_cp_set_default_system_parameters {
 	uint8_t  parameters[0]; /* mgmt_system_parameter_tlv */
 } __packed;
 
+#define MGMT_ADV_MONITOR_FEATURE_MASK_OR_PATTERNS	(1 << 0)
+
+#define MGMT_OP_READ_ADV_MONITOR_FEATURES	0x0051
+struct mgmt_rp_read_adv_monitor_features {
+	uint32_t supported_features;
+	uint32_t enabled_features;
+	uint16_t max_num_handles;
+	uint8_t max_num_patterns;
+	uint16_t num_handles;
+	uint16_t handles[0];
+}  __packed;
+
+struct mgmt_adv_pattern {
+	uint8_t ad_type;
+	uint8_t offset;
+	uint8_t length;
+	uint8_t value[31];
+} __packed;
+
+#define MGMT_OP_ADD_ADV_MONITOR			0x0052
+struct mgmt_cp_add_adv_monitor {
+	uint8_t pattern_count;
+	struct mgmt_adv_pattern patterns[0];
+} __packed;
+struct mgmt_rp_add_adv_patterns_monitor {
+	uint16_t monitor_handle;
+} __packed;
+
+#define MGMT_OP_REMOVE_ADV_MONITOR		0x0053
+struct mgmt_cp_remove_adv_monitor {
+	uint16_t monitor_handle;
+} __packed;
+struct mgmt_rp_remove_adv_monitor {
+	uint16_t monitor_handle;
+} __packed;
+
 #define MGMT_EV_CMD_COMPLETE		0x0001
 struct mgmt_ev_cmd_complete {
 	uint16_t opcode;
@@ -875,6 +911,16 @@ struct mgmt_ev_exp_feature_changed {
 	uint32_t flags;
 } __packed;
 
+#define MGMT_EV_ADV_MONITOR_ADDED	0x002b
+struct mgmt_ev_adv_monitor_added {
+	uint16_t monitor_handle;
+}  __packed;
+
+#define MGMT_EV_ADV_MONITOR_REMOVED	0x002c
+struct mgmt_ev_adv_monitor_removed {
+	uint16_t monitor_handle;
+}  __packed;
+
 static const char *mgmt_op[] = {
 	"<0x0000>",
 	"Read Version",
@@ -953,6 +999,9 @@ static const char *mgmt_op[] = {
 	"Set Experimental Feature",
 	"Read Default System Configuration",
 	"Set Default System Configuration",
+	"Read Advertisement Monitor Features",
+	"Add Advertisement Monitor",
+	"Remove Advertisement Monitor",
 };
 
 static const char *mgmt_ev[] = {
@@ -996,6 +1045,8 @@ static const char *mgmt_ev[] = {
 	"Extended Controller Information Changed",
 	"PHY Configuration Changed",
 	"Experimental Feature Changed",
+	"Advertisement Monitor Added",			/* 0x002b */
+	"Advertisement Monitor Removed",
 };
 
 static const char *mgmt_status[] = {
