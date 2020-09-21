3c1d875bf45d436a09e5e428c213f48222d6d8da
Tedd Ho-Jeong An <tedd.an@intel.com>
2020-09-21 12:00:28 -0700

    lib: Add SPDX License Identifier
    This patch adds SPDX License Identifier and removes the license text.
    
    -------------------------------------
           License            COUNT
    -------------------------------------
     GPL-2.0-or-later     :     18
     GPL-2.0-only         :      1
    
    License: GPL-2.0-or-later
       lib/sco.h
       lib/sdp.c
       lib/a2mp.h
       lib/uuid.h
       lib/bluetooth.h
       lib/hidp.h
       lib/rfcomm.h
       lib/hci.c
       lib/sdp.h
       lib/sdp_lib.h
       lib/bluetooth.c
       lib/mgmt.h
       lib/hci.h
       lib/uuid.c
       lib/l2cap.h
       lib/bnep.h
       lib/hci_lib.h
       lib/cmtp.h
    
    License: GPL-2.0-only
       lib/amp.h

--- lib/amp.h
+++ lib/amp.h
@@ -1,18 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  *
  *	BlueZ - Bluetooth protocol stack for Linux
  *
  *	Copyright (C) 2010-2011 Code Aurora Forum.  All rights reserved.
  *	Copyright (C) 2012 Intel Corporation.
- *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License version 2 and
- *	only version 2 as published by the Free Software Foundation.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
  *
  */
 
