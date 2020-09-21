e59926d7628a341a774aec541602e4ccf36bc5fb
Tedd Ho-Jeong An <tedd.an@intel.com>
2020-09-21 12:00:34 -0700

    profiles: Add SPDX License Identifier
    This patch adds SPDX License Identifier and removes the license text.
    
    -------------------------------------
           License            COUNT
    -------------------------------------
     GPL-2.0-or-later     :     72
     LGPL-2.1-or-later    :      6
    
    License: GPL-2.0-or-later
       profiles/deviceinfo/dis.c
       profiles/deviceinfo/deviceinfo.c
       profiles/health/hdp_types.h
       profiles/health/mcap.c
       profiles/health/hdp_manager.c
       profiles/health/hdp_main.c
       profiles/health/mcap.h
       profiles/health/hdp_util.h
       profiles/health/hdp_manager.h
       profiles/health/hdp_util.c
       profiles/health/hdp.c
       profiles/health/hdp.h
       profiles/network/server.c
       profiles/network/connection.h
       profiles/network/connection.c
       profiles/network/manager.c
       profiles/network/bnep.c
       profiles/network/server.h
       profiles/network/bnep.h
       profiles/battery/battery.c
       profiles/cups/cups.h
       profiles/cups/sdp.c
       profiles/cups/main.c
       profiles/cups/spp.c
       profiles/cups/hcrp.c
       profiles/scanparam/scpp.c
       profiles/scanparam/scan.c
       profiles/audio/transport.h
       profiles/audio/avdtp.h
       profiles/audio/source.c
       profiles/audio/player.c
       profiles/audio/avctp.c
       profiles/audio/control.h
       profiles/audio/a2dp.c
       profiles/audio/source.h
       profiles/audio/transport.c
       profiles/audio/sink.c
       profiles/audio/avrcp.h
       profiles/audio/avdtp.c
       profiles/audio/a2dp.h
       profiles/audio/avrcp.c
       profiles/audio/media.h
       profiles/audio/avctp.h
       profiles/audio/control.c
       profiles/audio/sink.h
       profiles/audio/player.h
       profiles/audio/media.c
       profiles/sap/server.c
       profiles/sap/sap-dummy.c
       profiles/sap/main.c
       profiles/sap/sap.h
       profiles/sap/manager.c
       profiles/sap/server.h
       profiles/sap/manager.h
       profiles/iap/main.c
       profiles/gap/gas.c
       profiles/midi/midi.c
       profiles/midi/libmidi.c
       profiles/midi/libmidi.h
       profiles/input/server.c
       profiles/input/suspend.h
       profiles/input/uhid_copy.h
       profiles/input/device.c
       profiles/input/hog.c
       profiles/input/device.h
       profiles/input/hidp_defs.h
       profiles/input/manager.c
       profiles/input/suspend-none.c
       profiles/input/sixaxis.h
       profiles/input/hog-lib.c
       profiles/input/suspend-dummy.c
       profiles/input/server.h
    
    License: LGPL-2.1-or-later
       profiles/deviceinfo/dis.h
       profiles/battery/bas.h
       profiles/battery/bas.c
       profiles/scanparam/scpp.h
       profiles/audio/a2dp-codecs.h
       profiles/input/hog-lib.h

--- profiles/battery/bas.h
+++ profiles/battery/bas.h
@@ -1,23 +1,10 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
 /*
  *
  *  BlueZ - Bluetooth protocol stack for Linux
  *
  *  Copyright (C) 2014  Intel Corporation. All rights reserved.
  *
- *
- *  This library is free software; you can rebastribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is bastributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
 
