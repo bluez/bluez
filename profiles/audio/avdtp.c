e5a5fb8ecdb38577583c11c293c62b465e428152
morrish <morrish@perfectomobile.com>
2021-01-21 09:58:46 +0200

    avdtp: Correct possible dereferencing of null pointer in avdtp_abort

--- profiles/audio/avdtp.c
+++ profiles/audio/avdtp.c
@@ -3642,10 +3642,12 @@ int avdtp_abort(struct avdtp *session, struct avdtp_stream *stream)
 	struct seid_req req;
 	int ret;
 
-	if (!stream && session->discover) {
-		/* Don't call cb since it being aborted */
-		session->discover->cb = NULL;
-		finalize_discovery(session, ECANCELED);
+	if (!stream){
+        if (!session->discover) {
+            /* Don't call cb since it being aborted */
+            session->discover->cb = NULL;
+            finalize_discovery(session, ECANCELED);
+        }
 		return -EALREADY;
 	}
 
