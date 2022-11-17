818adf28e51877f32befab1a7faca314f89aa199
Brian Gix <brian.gix@intel.com>
2022-11-17 12:21:50 -0800

    tools: Use portable 64 bit data formats
    The isotest executable was using the non-portable %zd string formatter
    for loging 64 bit integers. This replaces them with PRId64.

--- tools/isotest.c
+++ tools/isotest.c
@@ -30,6 +30,7 @@
 #include <sys/uio.h>
 #include <linux/sockios.h>
 #include <time.h>
+#include <inttypes.h>
 
 #include "lib/bluetooth.h"
 #include "lib/hci.h"
@@ -589,12 +590,12 @@ static void send_wait(struct timespec *t_start, uint32_t us)
 	delta_us = us - TS_USEC(&t_diff);
 
 	if (delta_us < 0) {
-		syslog(LOG_INFO, "Send is behind: %zd us", delta_us);
+		syslog(LOG_INFO, "Send is behind: %" PRId64 " us", delta_us);
 		delta_us = 1000;
 	}
 
 	if (!quiet)
-		syslog(LOG_INFO, "Waiting (%zd us)...", delta_us);
+		syslog(LOG_INFO, "Waiting (%" PRId64 " us)...", delta_us);
 
 	usleep(delta_us);
 
