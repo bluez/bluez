6ee10e6df537cb7435d91ff38275306d8a2787f8
Marcel Holtmann <marcel@holtmann.org>
2023-10-04 08:08:48 +0200

    monitor: Add Bluetooth 5.4 to the version list

--- monitor/packet.c
+++ monitor/packet.c
@@ -2451,6 +2451,9 @@ void packet_print_version(const char *label, uint8_t version,
 	case 0x0c:
 		str = "Bluetooth 5.3";
 		break;
+	case 0x0d:
+		str = "Bluetooth 5.4";
+		break;
 	default:
 		str = "Reserved";
 		break;
