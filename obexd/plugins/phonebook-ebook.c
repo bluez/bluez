8fba2cd3b71267a9aad125f03ff39c0cfc2733bb
Andrew Sayers <kernel.org@pileofstuff.org>
2025-03-10 10:52:18 +0000

    phonebook-ebook: Fix typo: s/registery/registry/

--- obexd/plugins/phonebook-ebook.c
+++ obexd/plugins/phonebook-ebook.c
@@ -630,7 +630,7 @@ int phonebook_init(void)
 	/* Acquire ESource Registry */
 	registry = e_source_registry_new_sync(NULL, &gerr);
 	if (gerr != NULL) {
-		error("Unable to acquire registery: %s\n", gerr->message);
+		error("Unable to acquire registry: %s\n", gerr->message);
 		g_error_free(gerr);
 		return -1;
 	}
