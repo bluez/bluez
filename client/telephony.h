6b0469081d155fda2dff38eff9f7b843509fc6fc
Frédéric Danis <frederic.danis@collabora.com>
2025-05-18 09:13:15 +0200

    client/telephony: Add new submenu

---
+++ client/telephony.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ *
+ *  BlueZ - Bluetooth protocol stack for Linux
+ *
+ *  Copyright © 2025 Collabora Ltd.
+ *
+ *
+ */
+
+void telephony_add_submenu(void);
+void telephony_remove_submenu(void);
