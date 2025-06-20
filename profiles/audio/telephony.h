dc7fbc79b07a9e63a3f3dc5cbb64a56c0efe57b4
Frédéric Danis <frederic.danis@collabora.com>
2025-06-20 12:36:36 +0200

    audio/telephony: Add call multiparty property support

--- profiles/audio/telephony.h
+++ profiles/audio/telephony.h
@@ -115,3 +115,4 @@ void telephony_call_unregister_interface(struct call *call);
 
 void telephony_call_set_state(struct call *call, enum call_state state);
 void telephony_call_set_line_id(struct call *call, const char *line_id);
+void telephony_call_set_multiparty(struct call *call, bool multiparty);
