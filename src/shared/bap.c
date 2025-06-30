baedb651f920e566a84ced04183dea1cd220894e
Yang Li <yang.li@amlogic.com>
2025-06-30 16:02:40 +0800

    shared/bap: Add stream state check in stream_disable
    Add a state check so that stream_disable() is a no-op when the stream
    is not in ENABLING or STREAMING state. This prevents unexpected state
    transitions or redundant operations during cleanup.
    
    Signed-off-by: Yang Li <yang.li@amlogic.com>

--- src/shared/bap.c
+++ src/shared/bap.c
@@ -2134,10 +2134,14 @@ static uint8_t stream_disable(struct bt_bap_stream *stream, struct iovec *rsp)
 	/* Sink can autonomously transit to QOS while source needs to go to
 	 * Disabling until BT_ASCS_STOP is received.
 	 */
-	if (stream->ep->dir == BT_BAP_SINK)
+	if (stream->ep->dir == BT_BAP_SINK &&
+	   (stream->ep->state == BT_ASCS_ASE_STATE_ENABLING ||
+	    stream->ep->state == BT_ASCS_ASE_STATE_STREAMING))
 		stream_set_state(stream, BT_BAP_STREAM_STATE_QOS);
 
-	if (stream->ep->dir == BT_BAP_SOURCE)
+	if (stream->ep->dir == BT_BAP_SOURCE &&
+	   (stream->ep->state == BT_ASCS_ASE_STATE_ENABLING ||
+	    stream->ep->state == BT_ASCS_ASE_STATE_STREAMING))
 		stream_set_state(stream, BT_BAP_STREAM_STATE_DISABLING);
 
 	return 0;
