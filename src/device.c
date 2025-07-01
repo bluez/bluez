2f6b814a499f347c15e36cd0d1fa327ea11d4d5d
Bastien Nocera <hadess@hadess.net>
2025-07-01 12:14:24 +0200

    device: Use new DbusMessage error helper

--- src/device.c
+++ src/device.c
@@ -2340,8 +2340,7 @@ done:
 		}
 
 		g_dbus_send_message(dbus_conn,
-			btd_error_failed(dev->connect,
-					btd_error_str_bredr_conn_from_errno(err)));
+			btd_error_bredr_conn_from_errno(dev->connect, err));
 	} else {
 		/* Start passive SDP discovery to update known services */
 		if (dev->bredr && !dev->svc_refreshed && dev->refresh_discovery)
@@ -2698,8 +2697,7 @@ static DBusMessage *connect_profiles(struct btd_device *dev, uint8_t bdaddr_type
 	if (err < 0) {
 		if (err == -EALREADY)
 			return dbus_message_new_method_return(msg);
-		return btd_error_failed(msg,
-					btd_error_str_bredr_conn_from_errno(err));
+		return btd_error_bredr_conn_from_errno(msg, err);
 	}
 
 	dev->connect = dbus_message_ref(msg);
@@ -3002,10 +3000,10 @@ static void browse_request_complete(struct browse_req *req, uint8_t type,
 			if (err == 0)
 				goto done;
 		}
-		reply = btd_error_failed(req->msg,
-				bdaddr_type == BDADDR_BREDR ?
-				btd_error_str_bredr_conn_from_errno(err) :
-				btd_error_str_le_conn_from_errno(err));
+		if (bdaddr_type == BDADDR_BREDR)
+			reply = btd_error_bredr_conn_from_errno(req->msg, err);
+		else
+			reply = btd_error_le_conn_from_errno(req->msg, err);
 		goto done;
 	}
 
@@ -6282,8 +6280,7 @@ done:
 
 	if (device->connect) {
 		if (err < 0)
-			reply = btd_error_failed(device->connect,
-					btd_error_str_le_conn_from_errno(err));
+			reply = btd_error_le_conn_from_errno(device->connect, err);
 		else
 			reply = dbus_message_new_method_return(device->connect);
 
