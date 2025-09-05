2efd925db0b537c422d30283514e81816b673a03
Roman Smirnov <r.smirnov@omp.ru>
2025-09-05 16:49:39 +0300

    client/assistant: Use strnlen() instead of strlen()
    Using strlen() can lead to undefined behavior.

--- client/assistant.c
+++ client/assistant.c
@@ -261,7 +261,7 @@ static void assistant_set_bcode_cfg(const char *input, void *user_data)
 	if (!strcasecmp(input, "a") || !strcasecmp(input, "auto")) {
 		memset(cfg->qos.bcast.bcode, 0, BCODE_LEN);
 	} else {
-		if (strlen(input) > BCODE_LEN) {
+		if (strnlen(input, BCODE_LEN + 1) > BCODE_LEN) {
 			bt_shell_printf("Input string too long %s\n", input);
 			goto fail;
 		}
