c4c1a72f2470a0d0cd51281214eb0e2569bc4074
Roman Smirnov <r.smirnov@omp.ru>
2025-10-28 17:43:19 +0300

    battery: improve the display of the charge level
    The battery charge level may fluctuate due to uncalibrated
    sensors. Commit smooths out such fluctuations.
    
    The algorithm for determining uncalibrated sensors consists of
    finding the number of changes in charge direction (i.e., "spikes").
    If the number of spikes is zero, the device is charging or discharging.
    If there is one spike, it may mean that the device has started charging
    or has been disconnected from charging. If there are two or more spikes,
    this is a clear indication of an uncalibrated sensor.
    
    Check that the battery charge is fluctuating. If the battery charge
    is fluctuating, use the average charge value, otherwise use the current
    value.
    
    Fixes: https://github.com/bluez/bluez/issues/1612

--- src/battery.c
+++ src/battery.c
@@ -33,10 +33,15 @@
 #define BATTERY_PROVIDER_MANAGER_INTERFACE "org.bluez.BatteryProviderManager1"
 
 #define BATTERY_MAX_PERCENTAGE 100
+#define LAST_CHARGES_SIZE 8
+#define MAX_CHARGE_STEP 5
 
 struct btd_battery {
 	char *path; /* D-Bus object path */
 	uint8_t percentage; /* valid between 0 to 100 inclusively */
+	struct queue *last_charges; /* last charges received */
+	float avg_charge; /* average battery charge */
+	bool is_fluctuating; /* true, if the battery sensor fluctuates */
 	char *source; /* Descriptive source of the battery info */
 	char *provider_path; /* The provider root path, if any */
 };
@@ -92,6 +97,10 @@ static struct btd_battery *battery_new(const char *path, const char *source,
 	battery = new0(struct btd_battery, 1);
 	battery->path = g_strdup(path);
 	battery->percentage = UINT8_MAX;
+	battery->last_charges = queue_new();
+	battery->avg_charge = 0;
+	battery->is_fluctuating = false;
+
 	if (source)
 		battery->source = g_strdup(source);
 	if (provider_path)
@@ -104,6 +113,9 @@ static void battery_free(struct btd_battery *battery)
 {
 	if (battery->path)
 		g_free(battery->path);
+
+	if (battery->last_charges)
+		queue_destroy(battery->last_charges, NULL);
 
 	if (battery->source)
 		g_free(battery->source);
@@ -217,8 +229,49 @@ bool btd_battery_unregister(struct btd_battery *battery)
 	return true;
 }
 
+static void check_fluctuations(struct btd_battery *battery)
+{
+	const struct queue_entry *entry;
+	uint8_t spikes = 0;
+	int8_t step;
+	int8_t direction = 0;
+	int8_t prev_direction;
+	uint8_t *prev_charge;
+	uint8_t *next_charge;
+
+	for (entry = queue_get_entries(battery->last_charges); entry->next;
+	     entry = entry->next) {
+		prev_direction = direction;
+		prev_charge = entry->data;
+		next_charge = entry->next->data;
+		step = *next_charge - *prev_charge;
+
+		/*
+		 * The battery charge fluctuates too much,
+		 * which may indicate a battery problem, so
+		 * the actual value should be displayed.
+		 */
+		if (abs(step) >= MAX_CHARGE_STEP) {
+			battery->is_fluctuating = false;
+			return;
+		}
+
+		if (step > 0)
+			direction = 1;
+		else if (step < 0)
+			direction = -1;
+
+		if (direction != prev_direction && prev_direction)
+			spikes++;
+	}
+
+	battery->is_fluctuating = (spikes > 1) ? true : false;
+}
+
 bool btd_battery_update(struct btd_battery *battery, uint8_t percentage)
 {
+	uint8_t *p_percentage;
+
 	DBG("path = %s", battery->path);
 
 	if (!queue_find(batteries, NULL, battery)) {
@@ -230,6 +283,23 @@ bool btd_battery_update(struct btd_battery *battery, uint8_t percentage)
 		error("error updating battery: percentage is not valid");
 		return false;
 	}
+
+	if (!battery->avg_charge)
+		battery->avg_charge = percentage;
+
+	/* exponential smoothing */
+	battery->avg_charge = battery->avg_charge * 0.7 + percentage * 0.3;
+	p_percentage = new0(uint8_t, 1);
+	*p_percentage = percentage;
+	queue_push_tail(battery->last_charges, p_percentage);
+
+	if (queue_length(battery->last_charges) == LAST_CHARGES_SIZE) {
+		check_fluctuations(battery);
+		queue_pop_head(battery->last_charges);
+	}
+
+	if (battery->is_fluctuating)
+		percentage = battery->avg_charge;
 
 	if (battery->percentage == percentage)
 		return true;
