1812a687d59058bcf834b85bc1db4738fa23cf07
Pauli Virtanen <pav@iki.fi>
2025-11-05 20:26:52 +0200

    transport: always show A2DP Delay property as sink if DelayReporting
    The Delay property is currently not shown to sound server unless remote
    side has acked delay reporting.  However, when we are A2DP Sink, we
    reply to delay report requests with the current value, so the value
    logically exists and is active.  This behavior was apparently changed in
    commits 05f8bd489fd1 and 5d4efe960fd.
    
    Show Delay value for local A2DP Sink endpoint if it has DelayReporting.
    
    Fixes: https://github.com/bluez/bluez/issues/1541

--- profiles/audio/media.h
+++ profiles/audio/media.h
@@ -19,6 +19,7 @@ void media_unregister(struct btd_adapter *btd_adapter);
 
 struct a2dp_sep *media_endpoint_get_sep(struct media_endpoint *endpoint);
 const char *media_endpoint_get_uuid(struct media_endpoint *endpoint);
+bool media_endpoint_get_delay_reporting(struct media_endpoint *endpoint);
 uint8_t media_endpoint_get_codec(struct media_endpoint *endpoint);
 struct btd_adapter *media_endpoint_get_btd_adapter(
 					struct media_endpoint *endpoint);
