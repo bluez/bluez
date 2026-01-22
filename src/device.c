bluez/bluez

commit fa7828bddd2164408535a9ac45095564e9ebbeea

transport: Fix not being able to initialize volume properly
In case AVRCP is connected first and
media_transport_update_device_volume is called without any media_player
being available the volume setting would be lost and Transport.Volume
won't be available, so this introduces btd_device_{set,get}_volume
helpers which is used to store the volume temporarely so
media_player_get_device_volume is able to restore it when the transport
is created.

Fixes: https://github.com/bluez/bluez/issues/335

src/device.c
@@ -274,6 +274,8 @@ struct btd_device {
 	guint		store_id;
 
 	time_t		name_resolve_failed_time;
+
+	int8_t		volume;
 };
 
 static const uint16_t uuid_list[] = {
@@ -4334,6 +4336,7 @@ static struct btd_device *device_new(struct btd_adapter *adapter,
 		return NULL;
 
 	device->tx_power = 127;
+	device->volume = -1;
 
 	device->db = gatt_db_new();
 	if (!device->db) {
@@ -7302,4 +7305,14 @@ void btd_device_init(void)
 void btd_device_cleanup(void)
 {
 	btd_service_remove_state_cb(service_state_cb_id);
+}
+
+void btd_device_set_volume(struct btd_device *device, int8_t volume)
+{
+	device->volume = volume;
+}
+
+int8_t btd_device_get_volume(struct btd_device *device)
+{
+	return device->volume;
 }

profiles/audio/media.c
@@ -456,11 +456,11 @@ int8_t media_player_get_device_volume(struct btd_device *device)
 
 	target_player = avrcp_get_target_player_by_device(device);
 	if (!target_player)
-		return -1;
+		goto done;
 
 	adapter = find_adapter(device);
 	if (!adapter)
-		return -1;
+		goto done;
 
 	for (l = adapter->players; l; l = l->next) {
 		struct media_player *mp = l->data;
@@ -469,7 +469,9 @@ int8_t media_player_get_device_volume(struct btd_device *device)
 			return mp->volume;
 	}
 
-	return -1;
+done:
+	/* If media_player doesn't exists use device_volume */
+	return btd_device_get_volume(device);
 }
 
 static gboolean set_configuration(struct media_endpoint *endpoint,

profiles/audio/transport.c
@@ -949,6 +949,7 @@ int8_t media_transport_get_device_volume(struct btd_device *dev)
 	if (dev == NULL)
 		return -1;
 
+	/* Attempt to locate the transport to get its volume */
 	for (l = transports; l; l = l->next) {
 		struct media_transport *transport = l->data;
 		if (transport->device != dev)
@@ -959,7 +960,8 @@ int8_t media_transport_get_device_volume(struct btd_device *dev)
 			return media_transport_get_volume(transport);
 	}
 
-	return 0;
+	/* If transport volume doesn't exists use device_volume */
+	return btd_device_get_volume(dev);
 }
 
 void media_transport_update_device_volume(struct btd_device *dev,
@@ -970,13 +972,19 @@ void media_transport_update_device_volume(struct btd_device *dev,
 	if (dev == NULL || volume < 0)
 		return;
 
+	/* Attempt to locate the transport to set its volume */
 	for (l = transports; l; l = l->next) {
 		struct media_transport *transport = l->data;
 		if (transport->device != dev)
 			continue;
 
 		/* Volume is A2DP only */
-		if (media_endpoint_get_sep(transport->endpoint))
+		if (media_endpoint_get_sep(transport->endpoint)) {
 			media_transport_update_volume(transport, volume);
+			return;
+		}
 	}
+
+	/* If transport volume doesn't exists add to device_volume */
+	btd_device_set_volume(dev, volume);
 }

src/device.h
@@ -180,3 +180,6 @@ bool btd_device_all_services_allowed(struct btd_device *dev);
 void btd_device_update_allowed_services(struct btd_device *dev);
 void btd_device_init(void);
 void btd_device_cleanup(void);
+
+void btd_device_set_volume(struct btd_device *dev, int8_t volume);
+int8_t btd_device_get_volume(struct btd_device *dev);

commit 986b05c0be675126da8b9c290ea33d3325b8a9cd

device: Initialize device volume with a valid value
When AVRCP is connected earlier than AVDTP, this invalid device volume
causes the A2DP sink to reject the peer's registration for the
EVENT_VOLUME_CHANGED notification. As a result, subsequent attempts to
set the volume fail.

Fixes: fa7828bddd21 ("transport: Fix not being able to initialize volume properly")
Signed-off-by: Jinwang Li <jinwang.li@oss.qualcomm.com>

src/device.c
@@ -4948,7 +4948,7 @@ static struct btd_device *device_new(struct btd_adapter *adapter,
 		return NULL;
 
 	device->tx_power = 127;
-	device->volume = -1;
+	device->volume = 127;
 	device->wake_id = -1U;
 
 	device->db = gatt_db_new();