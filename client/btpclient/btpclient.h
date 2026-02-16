7170722bb027f093ab92aefdc03eda15d5abf13a
Frédéric Danis <frederic.danis@collabora.com>
2026-02-16 20:31:35 +0100

    client/btpclient: Move btp core service in its own file
    Moving the btp services in their own files will simplify maintenance.

---
+++ client/btpclient/btpclient.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ *
+ *  BlueZ - Bluetooth protocol stack for Linux
+ *
+ *  Copyright (C) 2011-2017  Intel Corporation. All rights reserved.
+ *
+ */
+
+bool gap_register_service(void);
+void gap_unregister_service(void);
+bool gap_is_service_registered(void);
---
+++ client/btpclient/core.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+/*
+ *
+ *  BlueZ - Bluetooth protocol stack for Linux
+ *
+ *  Copyright (C) 2011-2017  Intel Corporation. All rights reserved.
+ *
+ */
+
+void core_register_service(struct btp *btp);
