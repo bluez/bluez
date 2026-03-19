bluez/bluez

commit 5e5b46c5c0ccec25034e56c730e4d8ec719d60f0

adapter: Do not send empty default system parameter list
If we have no default system parameters to set, bail out without
sending the empty list to the kernel just to get an error back.

src/adapter.c
@@ -4957,6 +4957,9 @@ static void load_defaults(struct btd_adapter *adapter)
 	if (!load_le_defaults(adapter, list, &btd_opts.defaults.le))
 		goto done;
 
+	if (mgmt_tlv_list_size(list) == 0)
+		goto done;
+
 	err = mgmt_send_tlv(adapter->mgmt, MGMT_OP_SET_DEF_SYSTEM_CONFIG,
 			adapter->dev_id, list, NULL, NULL, NULL);
 

src/shared/mgmt.c
@@ -686,6 +686,11 @@ void mgmt_tlv_list_free(struct mgmt_tlv_list *tlv_list)
 	free(tlv_list);
 }
 
+uint16_t mgmt_tlv_list_size(struct mgmt_tlv_list *tlv_list)
+{
+	return tlv_list->size;
+}
+
 bool mgmt_tlv_add(struct mgmt_tlv_list *tlv_list, uint16_t type, uint8_t length,
 								void *value)
 {

src/shared/mgmt.h
@@ -36,6 +36,7 @@ typedef void (*mgmt_request_func_t)(uint8_t status, uint16_t length,
 
 struct mgmt_tlv_list *mgmt_tlv_list_new(void);
 void mgmt_tlv_list_free(struct mgmt_tlv_list *tlv_list);
+uint16_t mgmt_tlv_list_size(struct mgmt_tlv_list *tlv_list);
 bool mgmt_tlv_add(struct mgmt_tlv_list *tlv_list, uint16_t type, uint8_t length,
 								void *value);
 #define mgmt_tlv_add_fixed(_list, _type, _value) \

commit 46937fd52f67f7a9dca9fd1cc367dabb4f029715

adapter: Fix "Failed to set default system config" startup warning
When using a stock configuration, there will be nothing in the list of
configuration changes to apply to the Bluetooth adapter, which since
5e5b46c5c0cc ("adapter: Do not send empty default system parameter
list") would throw an error, as sending an empty list to the adapter
didn't generate one.

bluetoothd[3291147]: Bluetooth daemon 5.86
bluetoothd[3291147]: Starting SDP server
bluetoothd[3291147]: Using external plugins is not officially supported.
bluetoothd[3291147]: Consider upstreaming your plugins into the BlueZ project.
bluetoothd[3291147]: Bluetooth management interface 1.23 initialized
bluetoothd[3291147]: Battery Provider Manager created
bluetoothd[3291147]: Failed to set default system config for hci0

Fixes: 5e5b46c5c0cc ("adapter: Do not send empty default system parameter list")

src/adapter.c
@@ -4963,8 +4963,11 @@ static void load_defaults(struct btd_adapter *adapter)
 	if (!load_le_defaults(adapter, list, &btd_opts.defaults.le))
 		goto done;
 
-	if (mgmt_tlv_list_size(list) == 0)
-		goto done;
+	/* No changes from defaults */
+	if (mgmt_tlv_list_size(list) == 0) {
+		mgmt_tlv_list_free(list);
+		return;
+	}
 
 	err = mgmt_send_tlv(adapter->mgmt, MGMT_OP_SET_DEF_SYSTEM_CONFIG,
 			adapter->dev_id, list, NULL, NULL, NULL);