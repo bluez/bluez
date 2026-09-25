f6e6c73ab1a4e446604d33c1f8a7bdb0a36ccc74
Frédéric Danis <frederic.danis.oss@gmail.com>
2026-09-25 15:25:27 +0200

    shared/hfp: Add codecs support
    This send the AT+BAC command with the supported codec list during
    SLC connection.
    
    On reception of +BCS event to select a codec to use it replies with
    the same codec if it is available, or re-send the AT+BAC to re-trigger
    codec selection.

--- src/shared/hfp.h
+++ src/shared/hfp.h
@@ -220,6 +220,9 @@ struct hfp_hf_callbacks {
 	void (*call_line_id_updated)(uint id, const char *number, uint type,
 							void *user_data);
 	void (*call_mpty_updated)(uint id, bool mpty, void *user_data);
+	uint8_t (*get_codecs)(uint8_t *codecs, uint8_t max_codecs,
+							void *user_data);
+	bool (*select_codec)(uint8_t codec, void *user_data);
 };
 
 struct hfp_hf *hfp_hf_new(int fd);
@@ -268,5 +271,9 @@ bool hfp_hf_call_answer(struct hfp_hf *hfp, uint id,
 				hfp_response_func_t resp_cb,
 				void *user_data);
 bool hfp_hf_call_hangup(struct hfp_hf *hfp, uint id,
+				hfp_response_func_t resp_cb,
+				void *user_data);
+
+bool hfp_hf_request_codec_connection(struct hfp_hf *hfp,
 				hfp_response_func_t resp_cb,
 				void *user_data);
